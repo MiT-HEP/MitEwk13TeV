4df18d9cedb3980cc5bd889503203cf3fe7688bb
Catherine <cmedlock@mit.edu>
2015-06-23 14:56:09 +0200

    Adjusted plotting function to make customization easier

--- Efficiency/TagAndProbe/runEleEff.sh
+++ Efficiency/TagAndProbe/runEleEff.sh
@@ -3,38 +3,34 @@
 #==============================================================
 
 # directory of probes ntuples
-NTUPLEDIR=/afs/cern.ch/work/j/jlawhorn/public/wz-efficiency
+NTUPLEDIR=/afs/cern.ch/work/c/cmedlock/wz-efficiency
 OUTPUTDIR="."
 
 #
 # Electron efficiencies
 #
-root -l -b -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleL1Eff/probes.root\",\"${OUTPUTDIR}/Zee_EleL1Eff/analysis\",\"png\",0,0,0\)
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff/analysis\",\"png\",0,0,0\)
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"${OUTPUTDIR}/May23_EleHLTEff/analysis\",\"png\",0,0,0\)
-##root -l -q plotEff.C+\(\"elhlt_highpt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"May23_EleHLTEff/190949\",\"png\",0,0,0,\"\",190949,190949\)
+#root -l -b -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleL1Eff/probes.root\",\"${OUTPUTDIR}/Zee_EleL1Eff\",\"png\",0,0,0,\"Supercluster\",\"L1 trigger\",0.8,1.03\)
 
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff/analysis\",\"png\",1,0,0\)
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",2,1,2,2,\"${NTUPLEDIR}/May23_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/May23_EleGsfSelEff/analysis\",\"png\",1,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff\",\"png\",0,0,0,\"Supercluster\",\"trigger\",0.8,1.03\)
+
+#root -l -b -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleSelEff\",\"png\",0,0,0,\"Supercluster\",\"ID+Iso\",0.8,1.03\)
+
+#root -l -b -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfEff\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.8,1.03\)
+
+root -l -b -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff\",\"png\",0,0,0,\"Supercluster\",\"GSF+ID+Iso\",0.5,1.02\)
 
 #
 # Electron (+) efficiencies
 #
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff_pos/analysis\",\"png\",0,0,1\)
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"${OUTPUTDIR}/May23_EleHLTEff_pos/analysis\",\"png\",0,0,1\)
-#root -l -q plotEff.C+\(\"elhlt_highpt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"${OUTPUTDIR}/May23_EleHLTEff_pos/190949\",\"png\",0,0,1,\"\",190949,190949\)
+#root -l -b -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff_pos\",\"png\",0,0,1,\"Supercluster\",\"trigger\"\)
 
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff_pos/analysis\",\"png\",0,0,1\)
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",2,1,2,2,\"${NTUPLEDIR}/May23_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/May23_EleGsfSelEff_pos/analysis\",\"png\",0,0,1,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff_pos\",\"png\",1,0,1,\"Supercluster\",\"GSF+ID+Iso\"\)
 
 #
 # Electron (-) efficiencies
 #
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff_neg/analysis\",\"png\",0,0,-1\)
-#root -l -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"${OUTPUTDIR}/May23_EleHLTEff_neg/analysis\",\"png\",0,0,-1\)
-#root -l -q plotEff.C+\(\"elhlt_highpt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_EleHLTEff/probes.root\",\"${OUTPUTDIR}/May23_EleHLTEff_neg/190949\",\"png\",0,0,-1,\"\",190949,190949\)
+#root -l -b -q plotEff.C+\(\"elhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleHLTEff/probes.root\",\"${OUTPUTDIR}/Zee_EleHLTEff_neg\",\"png\",0,0,-1,\"Supercluster\",\"trigger\"\)
 
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff_neg/analysis\",\"png\",1,0,-1\)
-#root -l -q plotEff.C+\(\"elgsfsel.bins\",2,1,2,2,\"${NTUPLEDIR}/May23_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/May23_EleGsfSelEff_neg/analysis\",\"png\",1,0,-1,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"elgsfsel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zee_EleGsfSelEff/probes.root\",\"${OUTPUTDIR}/Zee_EleGsfSelEff_neg\",\"png\",1,0,-1,\"Supercluster\",\"GSF+ID+Iso\"\)
 
 rm *.so *.d
--- Efficiency/TagAndProbe/runMuEff.sh
+++ Efficiency/TagAndProbe/runMuEff.sh
@@ -3,62 +3,47 @@
 #==============================================================
 
 # directory of probes ntuples
-NTUPLEDIR=/afs/cern.ch/work/j/jlawhorn/public/wz-efficiency
+NTUPLEDIR=/afs/cern.ch/work/c/cmedlock/wz-efficiency
 
 #
 # Muon efficiencies
 #
-root -l -b -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuL1Eff/probes.root\",\"Zmm_MuL1Eff/analysis\",\"png\",0,0,0\)
-#root -l -b -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff/analysis\",\"png\",0,0,0\)
-#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_MuHLTEff/probes.root\",\"May23_MuHLTEff/analysis\",\"png\",0,0,0\)
+#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuL1Eff/probes.root\",\"Zmm_MuL1Eff\",\"png\",0,0,0,\"Muon\",\"L1 trigger\",0.7,1.03\)
 
-#root -l -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff/analysis\",\"png\",0,0,0\)
-#root -l -q plotEff.C+\(\"musel.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuSelEff/probes.root\",\"May23_MuSelEff/analysis\",\"png\",0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff\",\"png\",0,0,0,\"Muon\",\"trigger\",0.7,1.03\)
 
-#root -l -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff/analysis\",\"png\",1,0,0\)
-#root -l -q plotEff.C+\(\"mutrk.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuTrkEff/probes.root\",\"May23_MuTrkEff/analysis\",\"png\",1,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff\",\"png\",0,0,0,\"Muon\",\"selection\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff/analysis\",\"png\",1,0,0\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff/probes.root\",\"May23_MuStaEff/analysis\",\"png\",1,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff\",\"png\",1,0,0,\"Muon\",\"tracking\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso/analysis\",\"png\",1,0,0\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff_iso/probes.root\",\"May23_MuStaEff_iso/analysis\",\"png\",1,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\"\)
+#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff\",\"png\",0,0,0,\"Muon\",\"stand-alone (no iso)\",0.7,1.03\)
+
+#root -l -b -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso\",\"png\",1,0,0,\"Muon\",\"stand-alone\",0.7,1.02\)
 
 #
 # Muon (+) efficiencies
 #
-#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff_pos/analysis\",\"png\",0,0,1\)
-#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_MuHLTEff/probes.root\",\"May23_MuHLTEff_pos/analysis\",\"png\",0,0,1\)
+#root -l -b -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff_pos\",\"png\",0,0,1,\"Muon\",\"trigger\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff_pos/analysis\",\"png\",0,0,1\)
-#root -l -q plotEff.C+\(\"musel.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuSelEff/probes.root\",\"May23_MuSelEff_pos/analysis\",\"png\",0,0,1,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff_pos\",\"png\",0,0,1,\"Muon\",\"selection\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff_pos/analysis\",\"png\",1,0,1\)
-#root -l -q plotEff.C+\(\"mutrk.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuTrkEff/probes.root\",\"May23_MuTrkEff_pos/analysis\",\"png\",1,0,1,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff_pos\",\"png\",1,0,1,\"Muon\",\"tracking\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff_pos/analysis\",\"png\",1,0,1\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff/probes.root\",\"May23_MuStaEff_pos/analysis\",\"png\",1,0,1,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\"\)
+#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff_pos\",\"png\",1,0,1,\"Muon\",\"stand-alone (no iso)\",0.7,1.03\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso_pos/analysis\",\"png\",1,0,1\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff_iso/probes.root\",\"May23_MuStaEff_iso_pos/analysis\",\"png\",1,0,1,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\"\)
-
+#root -l -b -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso_pos\",\"png\",1,0,1,\"Muon\",\"stand-alone\",0.7,1.02\)
 
 #
 # Muon (-) efficiencies
 #
-#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff_neg/analysis\",\"png\",0,0,-1\)
-#root -l -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/May23_MuHLTEff/probes.root\",\"May23_MuHLTEff_neg/analysis\",\"png\",0,0,-1\)
+#root -l -b -q plotEff.C+\(\"muhlt.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuHLTEff/probes.root\",\"Zmm_MuHLTEff_neg\",\"png\",0,0,-1,\"Muon\",\"trigger\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff_neg/analysis\",\"png\",0,0,-1\)
-#root -l -q plotEff.C+\(\"musel.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuSelEff/probes.root\",\"May23_MuSelEff_neg/analysis\",\"png\",0,0,-1,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"musel.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuSelEff/probes.root\",\"Zmm_MuSelEff_neg\",\"png\",0,0,-1,\"Muon\",\"selection\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff_neg/analysis\",\"png\",1,0,-1\)
-#root -l -q plotEff.C+\(\"mutrk.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuTrkEff/probes.root\",\"May23_MuTrkEff_neg/analysis\",\"png\",1,0,-1,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\"\)
+#root -l -b -q plotEff.C+\(\"mutrk.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuTrkEff/probes.root\",\"Zmm_MuTrkEff_neg\",\"png\",1,0,-1,\"Muon\",\"tracking\",0.7,1.02\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff_neg/analysis\",\"png\",1,0,-1\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff/probes.root\",\"May23_MuStaEff_neg/analysis\",\"png\",1,0,-1,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\"\)
+#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff/probes.root\",\"Zmm_MuStaEff_neg\",\"png\",1,0,-1,\"Muon\",\"stand-alone (no iso)\",0.7,1.03\)
 
-#root -l -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso_neg/analysis\",\"png\",1,0,-1\)
-#root -l -q plotEff.C+\(\"musta.bins\",2,1,2,1,\"${NTUPLEDIR}/May23_MuStaEff_iso/probes.root\",\"May23_MuStaEff_iso_neg/analysis\",\"png\",1,0,-1,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\"\)
+root -l -b -q plotEff.C+\(\"musta.bins\",0,0,0,0,\"${NTUPLEDIR}/Zmm_MuStaEff_iso/probes.root\",\"Zmm_MuStaEff_iso_neg\",\"png\",1,0,-1,\"Muon\",\"standalone\",0.7,1.02\)
 
 rm *.so *.d
