8949055519a1f9cefbaaf325981a7a38fdd67410
Jay Lawhorn <jay@jaylawhorn.com>
2015-07-26 17:00:09 +0200

    update for mc->data

--- Utils/LeptonCorr.hh
+++ Utils/LeptonCorr.hh
@@ -1,33 +1,33 @@
 #ifndef EWKANA_UTILS_LEPTONCORR_HH
 #define EWKANA_UTILS_LEPTONCORR_HH
 
-Double_t getScaleCorr(const Double_t eta, const Int_t sigma)
+Double_t getEleScaleCorr(const Double_t eta, const Int_t sigma)
 {
   if (sigma==0) {
-    if      (fabs(eta) < 0.5)    { return 0.998; }
-    else if (fabs(eta) < 1.0)    { return 0.998; }
-    else if (fabs(eta) < 1.4442) { return 1.003; }
-    else if (fabs(eta) < 2.0)    { return 0.996; }
-    else                         { return 1.017; }
+    if      (fabs(eta) < 0.5)    { return 1.0/0.998; }
+    else if (fabs(eta) < 1.0)    { return 1.0/0.998; }
+    else if (fabs(eta) < 1.4442) { return 1.0/1.003; }
+    else if (fabs(eta) < 2.0)    { return 1.0/0.996; }
+    else                         { return 1.0/1.017; }
   }
   else if (sigma==1) {
-    if      (fabs(eta) < 0.5)    { return 0.998+0.001; }
-    else if (fabs(eta) < 1.0)    { return 0.998+0.002; }
-    else if (fabs(eta) < 1.4442) { return 1.003+0.001; }
-    else if (fabs(eta) < 2.0)    { return 0.996+0.001; }
-    else                         { return 1.017+0.001; }
+    if      (fabs(eta) < 0.5)    { return 1.0/(0.998+0.001); }
+    else if (fabs(eta) < 1.0)    { return 1.0/(0.998+0.002); }
+    else if (fabs(eta) < 1.4442) { return 1.0/(1.003+0.001); }
+    else if (fabs(eta) < 2.0)    { return 1.0/(0.996+0.001); }
+    else                         { return 1.0/(1.017+0.001); }
   }
   else if (sigma==-1) {
-    if      (fabs(eta) < 0.5)    { return 0.998-0.001; }
-    else if (fabs(eta) < 1.0)    { return 0.998-0.002; }
-    else if (fabs(eta) < 1.4442) { return 1.003-0.001; }
-    else if (fabs(eta) < 2.0)    { return 0.996-0.001; }
-    else                         { return 1.017-0.001; }
+    if      (fabs(eta) < 0.5)    { return 1.0/(0.998-0.001); }
+    else if (fabs(eta) < 1.0)    { return 1.0/(0.998-0.002); }
+    else if (fabs(eta) < 1.4442) { return 1.0/(1.003-0.001); }
+    else if (fabs(eta) < 2.0)    { return 1.0/(0.996-0.001); }
+    else                         { return 1.0/(1.017-0.001); }
   }
   else return -1.0;
 }
 
-Double_t getResCorr(const Double_t eta, const Int_t sigma)
+Double_t getEleResCorr(const Double_t eta, const Int_t sigma)
 {
   if (sigma==0) {
     if      (fabs(eta) < 0.5)    { return 0.8; }
