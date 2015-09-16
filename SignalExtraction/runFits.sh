81fefb3901b163e49b43f6521347db222cde29fa
Aram Apyan <arapyan@dtmit03.cern.ch>
2015-09-16 12:53:08 +0200

    signal extraction

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-HFLUMI=40.03
-PIXLUMI=40.03
+HFLUMI=41.8556
+PIXLUMI=41.8556
 LUMI=${HFLUMI}
 
-root -l -q fitWm.C+\(\"Wmunu\",${LUMI},0\)
+#root -l -q fitWm.C+\(\"Wmunu\",${LUMI},0\)
+#root -l -q fitWm_mc.C+\(\"Wmunu_mc\",${LUMI},0\)
 #root -l -q fitZmm.C+\(\"Zmumu\",${LUMI},0\)
 
-#root -l -q fitWe.C+\(\"Wenu\",${LUMI},0\)
+root -l -q fitWe.C+\(\"Wenu\",${LUMI},0\)
 #root -l -q fitZee.C+\(\"Zee\",${LUMI},0\)
 #root -l -q fitZee2.C+\(\"May23/Zee2\",${LUMI},0\)
 
-#root -l -q plotZmm.C+\(\"Zmumu\",${LUMI}\)
-#root -l -q  plotZee.C+\(\"Zee\",${LUMI}\)
+#root -l -q  plotZmm.C+\(\"Zmumu\",${LUMI}\)
+#root -l -q   plotZee.C+\(\"Zee\",${LUMI}\)
 
+#root -l -q postFitWe.C+\(\"Wenu_post\",${LUMI},0\)
+#root -l -q postFitWm.C+\(\"Wmunu_post\",${LUMI},0\)
 #rm *.so *.d
