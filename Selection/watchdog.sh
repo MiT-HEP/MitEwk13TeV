9b12366e4af332c7ab5939e8261401e6673b8db7
Andrea Marini <andrea.marini@cern.ch>
2016-02-11 12:36:16 +0100

    wd

--- Selection/watchdog.sh
+++ Selection/watchdog.sh
@@ -2,16 +2,16 @@
 
 WHAT="selectZmm"
 
-LIST=$@
-[ "X$LIST" == "X"] &&  LIST="selectZmm selectZmmGen"
+LIST="$@"
+[ "X$LIST" == "X" ] &&  LIST="selectZmm selectZmmGen"
 
 echo "LIST is '$LIST'"
 
-for WHAT in selectZmm selectZmmGen;
+for WHAT in $LIST ;
 do
    while sleep 3s;
    do
-   	ps aux | grep -v grep | grep "$USER" | grep $WHAT  || 
+   	ps aux | grep -v grep | grep "$USER" | grep $WHAT >& /dev/null || 
    		{ echo "Done $WHAT" ; echo "DONE" | mail -s "done $WHAT" $USER@cern.ch ;  break;   }
    done
 
