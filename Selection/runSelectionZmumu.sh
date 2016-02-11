734289041697a2fd2ac6f20302fdba29d3c0cdb2
Andrea Marini <andrea.marini@cern.ch>
2016-02-11 12:21:24 +0100

    echo stuff

--- Selection/runSelectionZmumu.sh
+++ Selection/runSelectionZmumu.sh
@@ -6,7 +6,9 @@ NTUPDIR=/afs/cern.ch/work/a/amarini/Bacon/Run2/wz_flat_76X
 # integrated luminosity for data
 LUMI=2263
 
+echo "->Running Zmm"
 root -l -q selectZmm.C+\(\"zmm_eos.conf\",\"${NTUPDIR}/Zmumu\",0\)
-#root -l -q selectZmmGen.C+\(\"zmmgen.conf\",\"${NTUPDIR}/ZmumuGen\",0\)
+echo "->Running ZmmGen"
+root -l -q selectZmmGen.C+\(\"zmmgen.conf\",\"${NTUPDIR}/ZmumuGen\",0\)
 
 rm *.so *.d
