f578b9a3a7b5f5b0cd449773eaecba8bfd619594
Andrea Marini <andrea.marini@cern.ch>
2016-02-11 12:15:08 +0100

    ntuple dir

--- Selection/runSelectionZee.sh
+++ Selection/runSelectionZee.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # output ntuple directory
-NTUPDIR=/afs/cern.ch/work/a/amarini/Bacon/Run2/wz_flat_76X_v2
+NTUPDIR=/afs/cern.ch/work/a/amarini/Bacon/Run2/wz_flat_76X
 
 # integrated luminosity for data
 LUMI=2263
