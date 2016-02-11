50738a9a7326976c0cbd869bf83f2e98347c6ee1
Andrea Marini <andrea.marini@cern.ch>
2016-02-11 14:00:14 +0100

    wd

--- Selection/watchdog.sh
+++ Selection/watchdog.sh
@@ -9,10 +9,11 @@ echo "LIST is '$LIST'"
 
 for WHAT in $LIST ;
 do
+   echo "-> Checking for $WHAT"
    while sleep 3s;
    do
    	ps aux | grep -v grep | grep "$USER" | grep $WHAT >& /dev/null || 
    		{ echo "Done $WHAT" ; echo "DONE" | mail -s "done $WHAT" $USER@cern.ch ;  break;   }
    done
-
+   sleep 1m;
 done
