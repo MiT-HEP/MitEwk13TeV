c2a72d3ef3bcb3474e2a6501d7c160d77e494aa2
Katharina <katharina.bierwagen@cern.ch>
2016-02-25 12:29:38 +0100

    Updated luminosity value

--- SignalExtraction/runFitsZmm.sh
+++ SignalExtraction/runFitsZmm.sh
@@ -4,7 +4,7 @@
 NTUPDIR=/afs/cern.ch/work/a/amarini/Bacon/Run2/wz_flat_76X_newpu
 
 # integrated luminosity for data
-LUMI=2263
+LUMI=2318.3
 
 root -b -l -q  plotZmm.C+\(\"${NTUPDIR}/Zmumu/ntuples\",\"Zmm\",${LUMI}\,0\)
 root -b -l -q  plotZmm.C+\(\"${NTUPDIR}/Zmumu/ntuples\",\"Zmm\",${LUMI}\,1\)
--- UnfoldingInput/createUnfoldingInputs.sh
+++ UnfoldingInput/createUnfoldingInputs.sh
@@ -4,7 +4,7 @@
 NTUPDIR=/afs/cern.ch/user/a/amarini/work/Bacon/Run2/wz_flat_76X_newpu
 
 # integrated luminosity for data
-LUMI=2263
+LUMI=2318.3
 
 root -l -q  plotZmmGen.C+\(\"zmmgen.conf\",\"${NTUPDIR}/ZmumuGen/ntuples\",\"Zmm\",${LUMI}\)
 root -l -q  plotZmmGenResScaleUncert.C+\(\"${NTUPDIR}/ZmumuGen/ntuples\",\"Zmm\",${LUMI}\)
