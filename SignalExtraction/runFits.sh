ea0fd94b17986329c14ad4105ca860ca8ad278ea
Stephanie <s.brandt@cern.ch>
2016-06-10 12:39:57 +0200

    update signal extraction

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -4,11 +4,11 @@
 NTUPDIR=/afs/cern.ch/work/s/sabrandt/public/SM/newBacon/
 
 # integrated luminosity for data
-LUMI=2215
+LUMI=2318
 
 #root -l -q fitWm.C+\(\"Wmunu\",${LUMI},0\)
-#root -l -q fitWm_mc.C+\(\"Wmunu_mc\",${LUMI},0\)
-root -l -q fitZm.C+\(\"Zmumu_newBacon_lowPt\",${LUMI},0\)
+root -l -q fitWm.C+\(\"Wmunu_newBaconCorr\",${LUMI},0\)
+# root -l -q fitZm.C+\(\"Zmumu_newBacon_lowPt\",${LUMI},0\)
 
 #root -l -q fitWe.C+\(\"Wenu\",${LUMI},0\)
 #root -l -q fitZee.C+\(\"Zee\",${LUMI},0\)
