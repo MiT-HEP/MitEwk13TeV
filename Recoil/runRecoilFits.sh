e6afffa013a93c4e656cb562f020637c0f5cc8e2
Stephanie <s.brandt@cern.ch>
2016-08-16 17:09:10 +0200

    cleaned some lines

--- Recoil/runRecoilFits.sh
+++ Recoil/runRecoilFits.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 # INPUTDIR=/afs/cern.ch/work/s/sabrandt/public/SM/
-INPUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/SM/newBacon
+INPUTDIR=/afs/cern.ch/work/a/arapyan/public/flat_ntuples/
 #/data/blue/Bacon/Run2/wz_flat
 
 # muons
-#  root -l -q fitRecoilZmm3.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,1,\"u1\",\"u2\",\"pf\",\"ZmmDataPF\"\) #Zmumu data selection
-#   root -l -q fitRecoilZmm3.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"pf\",\"ZmmMCPF\"\) #Zmumu data selection
+#  root -l -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,1,\"u1\",\"u2\",\"pf\",\"ZmmDataPF\"\) #Zmumu data selection
+#   root -l -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"pf\",\"ZmmMCPF\"\) #Zmumu data selection
 # root -l -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",2,2,1,\"ppU1\",\"ppU2\",\"t1pf\",\"ZmmData\"\) #Zmumu data selection
-# root -l -q fitRecoilWm3.C+\(\"${INPUTDIR}/Wmunu/ntuples/\",3,3,1,1,0,\"u1\",\"u2\",\"pf\",\"WmpMCPF\"\) #Wmunu data selection
-root -l -q fitRecoilWm3.C+\(\"${INPUTDIR}/Wmunu/ntuples/\",3,3,1,-1,0,\"u1\",\"u2\",\"pf\",\"WmmMCPF\"\) #Wmunu data selection
+root -l -q fitRecoilWm.C+\(\"${INPUTDIR}/Wmunu/ntuples/\",3,3,1,1,0,\"puppiU1\",\"puppiU2\",\"puppi\",\"WmpMCPuppi_PileupUp\"\) #Wmunu data selection
+# root -l -q fitRecoilWm.C+\(\"${INPUTDIR}/Wmunu/ntuples/\",3,3,1,-1,0,\"puppiU1\",\"puppiU2\",\"puppi\",\"WmmMCPuppi_PileupDown\"\) #Wmunu data selection
  #root -l -q fitRecoilWm.C+\(\"${INPUTDIR}/Wmunu/ntuples/\",2,2,1,-1,0,\"mvaU1\",\"mvaU2\",\"mva\",\"WmmMC\"\) #Wmunu signal MC -
-
+ 
 # electrons
 #   root -l -q fitRecoilZee.C+\(\"${INPUTDIR}/Zee/ntuples/data_select.raw.root\",2,2,1,\"mvaU1\",\"mvaU2\",\"mva\",\"ZeeData\"\) #Zmumu data selection
  #root -l -q fitRecoilZee.C+\(\"${INPUTDIR}/Zee/ntuples/zee_select.root\",2,2,1,\"ZeeMC\"\) #Zee signal MC selection
