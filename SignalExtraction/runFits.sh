5eaa28778aaf6820a19af7c707c8ef4ce2f50e35
Stephanie Akemi Brandt <sabrandt@mit.edu>
2016-11-11 09:53:48 +0100

    updated script

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -4,9 +4,26 @@
 NTUPDIR=/afs/cern.ch/work/s/sabrandt/public/SM/newBacon/
 
 # integrated luminosity for data
-LUMI=2308
+LUMI=2305
 LUMI2=96
 
+# root -l -q fitWm.C+\(\"testWmunu\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Central_Charge\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
+# root -l -q fitWm.C+\(\"Wmunu_Recoil_RooKeys\",${LUMI},${LUMI2},0\) #recoil RooKeys
+# root -l -q fitWm.C+\(\"Wmunu_Pileup_Up\",${LUMI},${LUMI2},0\) #pileup Up
+# root -l -q fitWm.C+\(\"Wmunu_Pileup_Down\",${LUMI},${LUMI2},0\) #pileup Down
+# root -l -q fitWm.C+\(\"Wmunu_Ewk_Free\",${LUMI},${LUMI2},0\) #pileup Down
+root -l -q fitWm.C+\(\"Wmunu_QCD_Free\",${LUMI},${LUMI2},0\) #pileup Down
+
+# root -l -q fitWe.C+\(\"testWenu\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWe.C+\(\"Wenu_Central_Charge\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWe.C+\(\"Wenu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
+# root -l -q fitWe.C+\(\"Wenu_Recoil_RooKeys\",${LUMI},${LUMI2},0\) #recoil RooKeys
+# root -l -q fitWe.C+\(\"Wenu_Pileup_Up\",${LUMI},${LUMI2},0\) #pileup Up
+# root -l -q fitWe.C+\(\"Wenu_Pileup_Down\",${LUMI},${LUMI2},0\) #pileup Down
+ #################################################################3
+ 
 #  root -l -q fitWm.C+\(\"Wmunu_pileup_simultaneous_check\",${LUMI},${LUMI2},0\)
 # root -l -q fitWm.C+\(\"Wmunu_pileup_free_fixPtCut\",${LUMI},${LUMI2},0\)
 # root -l -q fitWm.C+\(\"AntiWmunu_WshapeDiff2\",${LUMI},${LUMI2},0\)
@@ -15,7 +32,6 @@ LUMI2=96
 #root -l -q fitWm_mc.C+\(\"Wmunu_mc\",${LUMI},0\)
 # root -l -q fitZm.C+\(\"Zmumu_PF_testToys\",${LUMI},0\)
 
-root -l -q fitWe.C+\(\"Wenu_test_pepe1_free\",${LUMI},${LUMI},0\)
 #root -l -q fitZee.C+\(\"Zee\",${LUMI},0\)
 #root -l -q fitZee2.C+\(\"May23/Zee2\",${LUMI},0\)
 
