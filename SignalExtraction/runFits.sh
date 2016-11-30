775f050833de8fa80e129f2c50ae86ac0504227b
Stephanie Akemi Brandt <sabrandt@mit.edu>
2016-11-30 20:10:29 +0100

    run fits

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -8,7 +8,7 @@ LUMI=2305
 LUMI2=96
 
 # root -l -q fitWm.C+\(\"testWmunu\",${LUMI},${LUMI2},0\) #central
-# root -l -q fitWm.C+\(\"Wmunu_Central_Charge\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Central_Charge_35GeV\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWm.C+\(\"Wmunu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
 # root -l -q fitWm.C+\(\"Wmunu_Recoil_RooKeys\",${LUMI},${LUMI2},0\) #recoil RooKeys
 # root -l -q fitWm.C+\(\"Wmunu_Pileup_Up\",${LUMI},${LUMI2},0\) #pileup Up
@@ -16,6 +16,9 @@ LUMI2=96
 # root -l -q fitWm.C+\(\"Wmunu_Ewk_Fix\",${LUMI},${LUMI2},0\) #pileup Down
 # root -l -q fitWm.C+\(\"Wmunu_QCD_Free\",${LUMI},${LUMI2},0\) #pileup Down
 
+root -l -q fitWe.C+\(\"Wenu_TestGaussEwk_35GeV\",${LUMI},${LUMI2},0\) #central
+
+# root -l -q fitWe.C+\(\"Wenu_Central_Charge_35GeV\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"testWenu\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"Wenu_Central_Charge\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"Wenu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
@@ -27,7 +30,7 @@ LUMI2=96
 #  root -l -q fitWm.C+\(\"Wmunu_pileup_simultaneous_check\",${LUMI},${LUMI2},0\)
 # root -l -q fitWm.C+\(\"Wmunu_pileup_free_fixPtCut\",${LUMI},${LUMI2},0\)
 # root -l -q fitWm.C+\(\"AntiWmunu_WshapeDiff2\",${LUMI},${LUMI2},0\)
-root -l -q fitBinsWm.C+\(\"Wmunu_etaBins_test\",${LUMI},${LUMI2},0\)
+# root -l -q fitBinsWm.C+\(\"Wmunu_etaBins_test\",${LUMI},${LUMI2},0\)
 # root -l -q fitZm.C+\(\"Zmumu_WLike\",${LUMI},0\)
 #root -l -q fitWm_mc.C+\(\"Wmunu_mc\",${LUMI},0\)
 # root -l -q fitZm.C+\(\"Zmumu_PF_testToys\",${LUMI},0\)
