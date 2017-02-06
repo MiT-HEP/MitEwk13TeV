19897a02dc416af4c491ddd98da5ea2220472e9c
Stephanie Akemi Brandt <sabrandt@mit.edu>
2017-02-06 18:09:03 +0100

    update run fits script

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -7,8 +7,16 @@ NTUPDIR=/afs/cern.ch/work/s/sabrandt/public/SM/newBacon/
 LUMI=2305
 LUMI2=96
 
-# root -l -q fitWm.C+\(\"testWmunu\",${LUMI},${LUMI2},0\) #central
-# root -l -q fitWm.C+\(\"Wmunu_Central_Charge\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"print_Bins\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_QCDFree_Print\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWe.C+\(\"Wenu_PF_bkg\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Puppi\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Puppi_MakeWorkspace\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWmPt.C+\(\"Wmunu_Puppi_lepPt_QCDData\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWe2.C+\(\"Wenu_Part2\",${LUMI},${LUMI2},0\) #central
+root -l -q fitWm.C+\(\"Wmunu_Default\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWe.C+\(\"Wenu_plot_eta3\",${LUMI},${LUMI2},0\) #central
+# root -l -q fitWm.C+\(\"Wmunu_Central_print_sf\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWm.C+\(\"Wmunu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
 # root -l -q fitWm.C+\(\"Wmunu_Recoil_RooKeys\",${LUMI},${LUMI2},0\) #recoil RooKeys
 # root -l -q fitWm.C+\(\"Wmunu_Pileup_Up\",${LUMI},${LUMI2},0\) #pileup Up
@@ -16,6 +24,11 @@ LUMI2=96
 # root -l -q fitWm.C+\(\"Wmunu_Ewk_Fix\",${LUMI},${LUMI2},0\) #pileup Down
 # root -l -q fitWm.C+\(\"Wmunu_QCD_Free\",${LUMI},${LUMI2},0\) #pileup Down
 
+# root -l -q fitWe.C+\(\"Wenu_EleID_QCD\",${LUMI},${LUMI2},0\) #central
+
+#  root -l -q fitWlikeZe.C+\(\"Zee_wLike_FixSel\",${LUMI},${LUMI2},0\) #central
+
+# root -l -q fitWe.C+\(\"Wenu_Central_Charge_35GeV\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"testWenu\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"Wenu_Central_Charge\",${LUMI},${LUMI2},0\) #central
 # root -l -q fitWe.C+\(\"Wenu_Recoil_Inclusive\",${LUMI},${LUMI2},0\) #recoil inclusive
@@ -23,25 +36,7 @@ LUMI2=96
 # root -l -q fitWe.C+\(\"Wenu_Pileup_Up\",${LUMI},${LUMI2},0\) #pileup Up
 # root -l -q fitWe.C+\(\"Wenu_Pileup_Down\",${LUMI},${LUMI2},0\) #pileup Down
  #################################################################3
- 
-#  root -l -q fitWm.C+\(\"Wmunu_pileup_simultaneous_check\",${LUMI},${LUMI2},0\)
-# root -l -q fitWm.C+\(\"Wmunu_pileup_free_fixPtCut\",${LUMI},${LUMI2},0\)
-# root -l -q fitWm.C+\(\"AntiWmunu_WshapeDiff2\",${LUMI},${LUMI2},0\)
-root -l -q fitBinsWm.C+\(\"Wmunu_etaBins_test\",${LUMI},${LUMI2},0\)
-# root -l -q fitZm.C+\(\"Zmumu_WLike\",${LUMI},0\)
-#root -l -q fitWm_mc.C+\(\"Wmunu_mc\",${LUMI},0\)
-# root -l -q fitZm.C+\(\"Zmumu_PF_testToys\",${LUMI},0\)
 
-#root -l -q fitZee.C+\(\"Zee\",${LUMI},0\)
-#root -l -q fitZee2.C+\(\"May23/Zee2\",${LUMI},0\)
+# root -l -q checkFP.C+\(\"checkFP_test\",${LUMI},0\) #central
 
-# root -l -q  plotZmm.C+\(\"${NTUPDIR}/Zmumu/ntuples\",\"Zmumu\",${LUMI}\,0\)
-# root -l -q  plotZmm.C+\(\"${NTUPDIR}/Zmumu/ntuples\",\"Zmumu\",${LUMI}\,1\)
-# root -l -q  plotZmmResScaleUncert.C+\(\"${NTUPDIR}/Zmumu/ntuples\",\"Zmumu\",${LUMI}\)
-#root -l -q   plotZee.C+\(\"Zee\",${LUMI}\)
-
-# root -l -q postFitWe.C+\(\"Wenu_post_simultaneous_fixLumi_fixPlots_fixCombine\",${LUMI},0\)
-# root -l -q postFitWm.C+\(\"Wmunu_post_free_fixPtCut\",${LUMI},0\)
-# root -l -q postFitTestCombine.C+\(\"Wmunu_test\",${LUMI},0\)
-# root -l -q postFitWm.C+\(\"WmunuPF_post_WShapeDiff\",${LUMI},0\)
 #rm *.so *.d
