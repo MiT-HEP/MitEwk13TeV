51d5672c670287b0f3eb7184157ef0cf77981f13
Aram Apyan <AramApyan@cern.ch>
2018-12-18 22:01:54 +0100

    lumi updates

--- Selection/runSelection.sh
+++ Selection/runSelection.sh
@@ -2,10 +2,10 @@
 
 # output ntuple directory
 #NTUPDIR=/data/blue/Bacon/Run2/wz_flat_diffxsec
-NTUPDIR=/data/t3home000/sabrandt/2018_12_15_fixTriggerFilters/
+NTUPDIR=/afs/cern.ch/work/a/arapyan/public/flat_new
 
 # integrated luminosity for data
-LUMI=212
+LUMI=213.1
 
 #root -l -q selectZmm.C+g\(\"zmm_eos.conf\",\"${NTUPDIR}/Zmumu\",0\)
 root -l -q selectZmm.C+\(\"zmm.conf\",\"${NTUPDIR}/Zmumu\",0\)
@@ -15,7 +15,7 @@ root -l -q selectZmm.C+\(\"zmm.conf\",\"${NTUPDIR}/Zmumu\",0\)
 #root -l -q rootlogon.plot.C plotZmm.C+\(\"zmm.conf\",\"${NTUPDIR}/Zmumu/ntuples\",\"Zmumu\",${LUMI}\)
 #root -l -q rootlogon.plot.C plotWm.C+\(\"wm.conf\",\"${NTUPDIR}/Wmunu/ntuples\",\"Wmunu\",${LUMI}\)
 
-# root -l -q selectZee.C+\(\"zee.conf\",\"${NTUPDIR}/Zee\",0\)
+#root -l -q selectZee.C+\(\"zee.conf\",\"${NTUPDIR}/Zee\",0\)
 #root -l -q selectWe.C+\(\"zee.conf\",\"${NTUPDIR}/Wenu\",0\)
 #root -l -q selectAntiWe.C+\(\"we.conf\",\"${NTUPDIR}/AntiWenu\",0\)
 #root -l -q rootlogon.plot.C plotZee.C+\(\"zee.conf\",\"${NTUPDIR}/Zee/ntuples\",\"Zee\",${LUMI}\)
