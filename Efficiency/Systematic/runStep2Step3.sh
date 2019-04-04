65865ca4e76b67ae2396cf2fce1ed46b4525a0a2
Stephanie Akemi Brandt <sabrandt@mit.edu>
2019-04-04 21:37:58 +0200

    bash script that directly controls the toy macro, gets submitted to condor

--- Efficiency/Systematic/runStep2Step3.sh
+++ Efficiency/Systematic/runStep2Step3.sh
@@ -8,10 +8,13 @@ NTOYS=$3
 FOLDER=$4
 EFFTYPE=$5
 CHARGE=$6
+POSTFIX=$7
+POSTFIX_alt=$8
 
 WORKDIR="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/MitEwk13TeV"
 # FILEDIR="/afs/cern.ch/work/s/sabrandt/public/LowPU_13TeV_Efficiency_v1/results"
-FILEDIR="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/MitEwk13TeV/Efficiency/testReweights_v2_2/results"
+# FILEDIR="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/MitEwk13TeV/Efficiency/testReweights_v2_2/results"
+FILEDIR="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/MitEwk13TeV/Efficiency/LowPU2017ID_13TeV_v0/results"
 
 CMSSW_BASE="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/"
 TOP="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/"
@@ -19,14 +22,23 @@ TOP="/afs/cern.ch/work/s/sabrandt/public/SM/LowPU/CMSSW_9_4_12/src/"
 # echo ${ClusterID}
 echo "running job w Cluster ID ${ClusterID}, binnum ${binnum}, NTOYS ${NTOYS}, FOLDER ${FOLDER}, EFFTYPE ${EFFTYPE}, Charge ${CHARGE}"
 
-POSTFIX=_POWxPythia_v1
-POSTFIX_alt=_POWxPhotos_v1
+## FSR alternative shape evaluation
+# POSTFIX=_POWxPythia_v1
+# POSTFIX_alt=_POWxPhotos_v1
+
+## MC alternative shape eval
+# POSTFIX=_aMCxPythia_v1
+# POSTFIX_alt=_minloxPythia_v1
+
+## BKG alternative shape eval
+# POSTFIX=_aMCxPythia_v1
+# POSTFIX_alt=_POWBKG_v1
 #
 # CMSSW_BASE="/afs/cern.ch/work/x/xniu/public/Lumi/Ele/CMSSW_7_6_3_patch2/src/"
 # TOP="$PWD"
 #
 BINVAR=etapt # probably don't need to change
-OUTPUTDIR=${FILEDIR}/${EFFTYPE}${POSTFIX}${POSTFIX_alt}/${CHARGE}/
+OUTPUTDIR=${FILEDIR}/TOYS/${EFFTYPE}${POSTFIX}${POSTFIX_alt}/${CHARGE}/FULL_TEST_v0
 # STAGEDIR=${TOP}/${EFFTYPE}/${CHARGE}/Step2Output/${POSTFIX}v${POSTFIX_alt}
 STAGEDIR=.
 # FOLDER=Zmm
@@ -46,9 +58,9 @@ mkdir -p ${OUTPUTDIR}
 #TOP="/afs/cern.ch/work/x/xniu/public/WZXSection/wz-efficiency"
 
 root -l -b << EOF
-gSystem->Load("${WORKDIR}/Efficiency/TagAndProbe/RooCMSShape_cc.so")
+gSystem->Load("${WORKDIR}/Efficiency/Systematic/RooCMSShape_cc.so")
+gSystem->Load("${WORKDIR}/Efficiency/Systematic/toyGenAndPull_C.so")
 toyGenAndPull("${DIR1}","${DIR2}","${DIR2}","${BINVAR}_${binnum}","${OUTPUTDIR}","pull_${binnum}",${binnum},${binnum},${NTOYS})
-gSystem->Load("${WORKDIR}/Efficiency/TagAndProbe/toyGenAndPull_C.so")
 .q
 EOF
 # root -l -b << EOF
