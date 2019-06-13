dff0c18a0bdf6b455051777ae52b1c4d5056ba2c
Stephanie Brandt <s.brandt@cern.ch>
2019-06-13 16:14:51 +0200

    added lines to make different types corrections

--- Recoil/runRecoilFits.sh
+++ Recoil/runRecoilFits.sh
@@ -16,10 +16,22 @@ LUMI=199.2
  # root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"met\",\"ZmmMCPF_lowPU\",${LUMI},0\)  #Zmumu data/ /BKG selection selection
 # root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"u1\",\"u2\",\"met\",\"ZmmDataPF_lowPU\",${LUMI},0\)  #Zmumu data/ /BKG selection selection
 
-root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmpMCPF\",${LUMI},0\)  #Wmp MC
-# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,-1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmmMCPF\",${LUMI},0\)  #Wmm MC
-# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmMCPF\",${LUMI},0\)  #Zmumu MC
-# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmDataPF\",${LUMI},0\)  #Zmumu data incl Bkg
+# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmpMCPF_v1\",${LUMI},0\)  #Wmp MC
+# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,-1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmmMCPF_v1\",${LUMI},0\)  #Wmm MC
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",2,2,1,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmMCPF_2G\",${LUMI},0\)  #Zmumu MC
+root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",2,2,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmDataPF_2G\",${LUMI},0\)  #Zmumu data incl Bkg
+
+# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,-1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmmMCPF_0-05_v1\",${LUMI},1\)  #Wmm MC
+# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,-1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmmMCPF_05-10_v1\",${LUMI},2\)  #Wmm MC
+# root -l -b -q fitRecoilWm.C+\(\"${INPUTDIR}\",3,3,1,-1,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_WmmMCPF_10-24_v1\",${LUMI},3\)  #Wmm MC
+
+# Eta-binned Zmm data and MC
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmMCPF_0-05_v1\",${LUMI},1\)  #Zmumu MC
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmMCPF_05-10_v1\",${LUMI},2\)  #Zmumu MC
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",3,3,1,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmMCPF_10-24_v1\",${LUMI},3\)  #Zmumu data incl Bkg
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmDataPF_0-05_v1\",${LUMI},1\)  #Zmumu data incl Bkg
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmDataPF_05-10_v1\",${LUMI},2\)  #Zmumu data incl Bkg
+# root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"u1\",\"u2\",\"met\",\"LowPU2017ID_13TeV_ZmmDataPF_10-24_v1\",${LUMI},3\)  #Zmumu data incl Bkg
 
 # muons
 # root -l -b -q fitRecoilZmm.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",3,3,0,\"puppiU1\",\"puppiU2\",\"puppi\",\"ZmmDataPuppi_bkgTopEWK_TEST\",${LUMI},0\)  #Zmumu data/ /BKG selection selection
