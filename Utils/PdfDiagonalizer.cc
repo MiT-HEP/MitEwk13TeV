5ee0bdbe2c2f7fc76ad302c7d9733ab953c13f7f
Stephanie Brandt <s.brandt@cern.ch>
2019-06-19 18:50:24 +0200

    cleaning

--- Utils/PdfDiagonalizer.cc
+++ Utils/PdfDiagonalizer.cc
@@ -69,57 +69,36 @@ PdfDiagonalizer::PdfDiagonalizer(const char *name, RooWorkspace *w, RooFitResult
 
 RooAbsPdf *PdfDiagonalizer::diagonalize(RooAbsPdf &pdf)
 {
-    std::cout << "start diagonalize function " << std::endl;
-    if (!pdf.dependsOn(parameters_)) return 0;
+  std::cout << "start diagonalize function " << std::endl;
+  if (!pdf.dependsOn(parameters_)) return 0;
 
-    // now do the customization
-    RooCustomizer custom(pdf, name_.c_str());
-    for (int i = 0, n = parameters_.getSize(); i < n; ++i) { 
-        if (pdf.dependsOn(*parameters_.at(i))) {
-            custom.replaceArg(*parameters_.at(i), *replacements_.at(i));
-			// std::cout << "params " << std::endl;
-			// parameters_.at(i)->Print();
-            // std::cout << " replace " << std::endl;
-			// replacements_.at(i)->Print();
-        }
+  // now do the customization
+  RooCustomizer custom(pdf, name_.c_str());
+  for (int i = 0, n = parameters_.getSize(); i < n; ++i) { 
+    if (pdf.dependsOn(*parameters_.at(i))) {
+      custom.replaceArg(*parameters_.at(i), *replacements_.at(i));
     }
+  }
 
-    RooAbsPdf *ret = dynamic_cast<RooAbsPdf *>(custom.build());
-    ret->SetName((std::string(pdf.GetName()) + "_" + name_).c_str());
-    std::cout << "print doDiago pdf value" << std::endl;
-    ret->Print();
-    return ret;
+  RooAbsPdf *ret = dynamic_cast<RooAbsPdf *>(custom.build());
+  ret->SetName((std::string(pdf.GetName()) + "_" + name_).c_str());
+  std::cout << "print doDiago pdf value" << std::endl;
+  ret->Print();
+  return ret;
 }
 
 RooAbsPdf *PdfDiagonalizer::diagonalizeWithEigenVariations(RooAbsPdf &pdf, RooFitResult &result, int index_eig, int nSigma)
 {
     
-    std::cout << "start diagonalizeWithEigenVarations function " << std::endl;
     // Check that the fit status is reasonable and that the covariance matrix is OK
-    if(result.status() != 0 || result.covQual() !=3){
-        std::cout << "bad fit result, not diagonalizing" << std::endl;
-        return &pdf;
-    }
-  
-    // pdf.Print();
-    // eigenVars_.Print();
-
-    // std::cout << "test -2" << std::endl;
+    if(result.status() != 0 || result.covQual() !=3){return &pdf;}
     TMatrixDSym cov(result.covarianceMatrix()); 
-    // std::cout << "test -1" << std::endl;
     TMatrixDSymEigen eigen(cov);
-
-    // std::cout << "test -0" << std::endl;
-    // const TMatrixD& vectors = eigen.GetEigenVectors();
+    
     const TVectorD& values  = eigen.GetEigenValues();
-
-    std::cout << "test 0+" << std::endl;
     if (!pdf.dependsOn(eigenVars_)) return 0;
-    std::cout << "test 0++" << std::endl;
 
     RooArgList  eigenShiftedVars_; // these are the diagonal parameters    
-
-    // std::cout << "test 1" << std::endl;
 
     // now do the customization
     RooCustomizer custom(pdf, name_.c_str());
@@ -134,8 +113,8 @@ RooAbsPdf *PdfDiagonalizer::diagonalizeWithEigenVariations(RooAbsPdf &pdf, RooFi
 
             double content=(dynamic_cast<RooAbsReal*>(eigenVars_.at(i)))->getVal();
             // std::cout << " name= " << myVar->GetName() << "  myVar= " <<  myVar->getVal() << "  central value= " << content;
-            content =+ nSigma*sqrt(values(i));
-            // if(i==index_eig) content =+ nSigma*sqrt(values(i));
+            // content =+ nSigma*sqrt(values(i));
+            if(i==index_eig) content =+ nSigma*sqrt(values(i));
             // std::cout << "  value inserted= " << content << std::endl;
             myVar->setVal(content);
 
@@ -151,69 +130,18 @@ RooAbsPdf *PdfDiagonalizer::diagonalizeWithEigenVariations(RooAbsPdf &pdf, RooFi
     // eigenVars_.Print();
 
     RooAbsPdf *ret = dynamic_cast<RooAbsPdf *>(custom.build());
-    // ret->SetName((std::string(pdf.GetName()) + "_" + name_).c_str());
     ret->SetName(std::string(pdf.GetName()).c_str());
-    std::cout << "value = " << ret->getVal() << std::endl;
-    std::cout << "log = " << ret->getLogVal() << std::endl;
+    // std::cout << "value = " << ret->getVal() << std::endl;
+    // std::cout << "log = " << ret->getLogVal() << std::endl;
     // std::cout << "eval = " << ret->evaluate() << std::endl;
 	// for(int i = 0; i < )
     
     if(ret->getVal() <= 0 || ret->getVal() > 1|| std::isnan(ret->getVal()) || !std::isfinite(ret->getVal())) {
-        std::cout << "blah returning PDF " << std::endl;
+        // std::cout << "blah returning PDF " << std::endl;
         return &pdf;
     }
-    std::cout << "print var" << std::endl;
-	ret->Print();
-    std::cout << "norm of eigenvar " << ret->getNorm() << std::endl;;
+    // std::cout << "print var" << std::endl;
+	// ret->Print();
+    // std::cout << "norm of eigenvar " << ret->getNorm() << std::endl;;
     return ret;
 }
-
-
-
-
-
-
-
-
-// RooAbsPdf* myVarPdf(RooAbsPdf &pdf, int whichParam, int nSigma, RooFitResult &result)
-// {
-  // if (!pdf.dependsOn(*eigenVars_)) return 0;
-
-  // /// take the eigen values again                                                                                                                                            
-  // TMatrixDSym cov(result.covarianceMatrix());
-  // TMatrixDSymEigen eigen(cov);
-  // const TVectorD& values  = eigen.GetEigenValues();
-
-  // /// make a shifted values                                                                                                                                                  
-  // RooArgList  eigenShiftedVars_; // these are the diagonal parameters                                                                                                        
-
-  // for (int i = 0, n = eigenVars_->getSize(); i < n; ++i) {
-    // //    RooRealVar * myVar=new RooRealVar(Form("name%d"),Form("name%d"),nSigma*values(i));                                                                                 
-    // RooRealVar myVar = RooRealVar("name","name",0);
-    // myVar.SetName(Form("%s_%d",eigenVars_->at(i)->GetName(),nSigma));
-    // myVar.setVal(nSigma*sqrt(values(i)));
-    // //    myVar.setVal(*eigenVars_->at(i) + nSigma*values(i));                                                                                                               
-    // cout << " name=" << myVar.GetName() << " myVar= " <<  myVar.getVal() << "central value=  " << *eigenVars_->at(i) << endl;
-    // eigenShiftedVars_.add(*&myVar);
-  // }
-
-  // // now do the customization                                                                                                                                                
-  // RooCustomizer custom(pdf, name_.c_str());
-  // for (int i = 0, n = eigenVars_->getSize(); i < n; ++i) {
-    // if (pdf.dependsOn(*eigenVars_->at(i))) {
-      // custom.replaceArg(*eigenVars_->at(i), *eigenVars_->at(i));
-      // /*                                                                                                                                                                     
-      // if(i==whichParam) {                                                                                                                                                    
-        // custom.replaceArg(*eigenVars_->at(i), *eigenShiftedVars_->at(i));                                                                                                    
-      // } else {                                                                                                                                                               
-        // custom.replaceArg(*eigenVars_->at(i), *eigenVars_->at(i));                                                                                                           
-      // }                                                                                                                                                                      
-      // */
-    // }
-  // }
-
-  // RooAbsPdf *ret = dynamic_cast<RooAbsPdf *>(custom.build());
-  // ret->SetName((std::string(pdf.GetName()) + "_" + name_).c_str());
-  // return ret;
-
-// }
