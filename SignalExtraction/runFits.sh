f9e7d015e8d53512eb09605e152204892f1efdf0
Stephanie Brandt <s.brandt@cern.ch>
2019-08-08 19:59:54 +0200

    updated location of files

--- SignalExtraction/runFits.sh
+++ SignalExtraction/runFits.sh
@@ -11,14 +11,15 @@ LUMI5=294.4 # Low PU 5 TeV 2017 ele trigger
 # LUMI2=199.270 # not used in Low PU atm
 
 # NTUPLEDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_13TeV_wRecoil_wStat_2G_eff
-NTUPLEDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_13TeV_wRecoil
+NTUPLEDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_13TeV_wGM
 # NTUPLEDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_5TeV_wRecoil
 OUT=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/SignalExtraction
 
-root -l -q fitWlnu.C+\(\"${OUT}/TEST_Wmunu13_fixRecoil_full_testRCsm_free_adjBkg_const2\",\"${NTUPLEDIR}\",\"Wmunu\",${LUMI13},${LUMI13}\);
+root -l -q fitWlnu.C+\(\"${OUT}/TEST_Wmunu13_wGM_fixHisto\",\"${NTUPLEDIR}\",\"Wmunu\",${LUMI13},${LUMI13}\);
 # root -l -q fitWlnu.C+\(\"${OUT}/TEST_Wmunu5_fixRecoil\",\"${NTUPLEDIR}\",\"Wmunu\",${LUMI5},${LUMI5}\);
 # root -l -q fitWlikeZm.C+\(\"TEST_WlikeZm_Inclusive_3G_ptRW\",$LUMI13,$LUMI13\);
 
-# root -l -q plotResiduals.C+\(\"TEST_2019June18_CheckFixedStaUnc_2GausStat_Keys_Eta_unzoom\",${LUMI13}\); 
+# # root -l -q plotResiduals.C+\(\"TEST_2019June18_CheckFixedStaUnc_2GausStat_Keys_Eta_unzoom\",${LUMI13}\); 
+# root -l -q plotWshapes.C+\(\"${OUT}/TEST_PlotSWs\",${LUMI13}\); 
 
 #rm *.so *.d
