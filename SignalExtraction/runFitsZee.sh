9f311774080326eedb7a073dae3d052a905c2aaf
Stephanie Brandt <s.brandt@cern.ch>
2019-10-16 17:09:49 +0200

    update locations

--- SignalExtraction/runFitsZee.sh
+++ SignalExtraction/runFitsZee.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 #ntuple directory
-# NTUPDIR=/afs/cern.ch/user/s/sabrandt/work/public/TEST_Zee_ScaleUp5sig
-# NTUPDIR=/afs/cern.ch/user/s/sabrandt/work/public/LowPU2017ID_13TeV_ele
-NTUPDIR=/afs/cern.ch/user/s/sabrandt/work/public/LowPU2017ID_13TeV_looseID_raw_rmTrig
-# NTUPDIR=/afs/cern.ch/user/s/sabrandt/work/public/LowPU2017ID_13TeV_ele_ecalEnergy
+
+# NTUPDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_13TeV
+NTUPDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_5TeV_Raw
 # NTUPDIR=/eos/cms/store/user/sabrandt/StandardModel/Ntuples2017GH/LowPU2017ID_13TeV
 OUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/SignalExtraction
 
@@ -12,7 +11,12 @@ OUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/SignalExtraction
 
 # integrated luminosity for data
 LUMI=199.2
+S13="13TeV"
+S5="5TeV"
 
 #root -l -q  plotZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"Zee\",${LUMI}\,0\)
-root -l -q  plotZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"${OUTDIR}/TEST_Zee_Pt32_raw_loose_noTrig_noCat\",${LUMI}\,1\)
+# root -l -q  plotZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"${OUTDIR}/TEST_Zee_LooseNoTrig_RawCorr_Compare_txt_SCALE_SMEAR_MINE\",${LUMI}\,1\)
+# root -l -q  plotZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"${OUTDIR}/TEST_corrEcalE_all_SCCorr_startRaw\",${LUMI}\,1\)
+# root -l -q  plotZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"${OUTDIR}/TEST_ZeeETA_test_noPF_v2\",\"${S13}\",${LUMI}\,1\)
+root -l -q  fitZee.C+\(\"${NTUPDIR}/Zee/ntuples\",\"${OUTDIR}/VALIDATE_Zee_5TeV_Loose_Raw\",\"${S13}\",${LUMI}\,1\)
 
