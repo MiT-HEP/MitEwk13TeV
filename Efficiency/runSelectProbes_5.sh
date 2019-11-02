946b82fd1344668cbc978fb537aba1c5ac0f8657
Stephanie Brandt <s.brandt@cern.ch>
2019-11-02 19:48:28 +0100

    5 tev version

---
+++ Efficiency/runSelectProbes_5.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+INPUTDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/LowPU2017ID_5TeV
+OUTPUTDIR=/afs/cern.ch/work/s/sabrandt/public/FilesSM2017GH/Efficiency/LowPU2017ID_5TeV/probes
+#
+# Select probes for muon efficiencies
+#
+
+# ## Zmm aMC@NLO with weights for the pythia and powheg w/ photos reweightings
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuHLTEff\",0,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuSITEff\",8,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuStaEff\",4,0,1\)
+
+# ## Tag PT cut
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuHLTEff_tagPt\",0,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuSITEff_tagPt\",8,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuStaEff_tagPt\",4,0,1\)
+
+## Zmm Minlo sample for one of the uncertainties
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu_minlo/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuHLTEff_minlo\",0,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu_minlo/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuSITEff_minlo\",8,0,1\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu_minlo/ntuples/zmm_select.raw.root\",\"${OUTPUTDIR}/Zmm/MC/MuStaEff_minlo\",4,0,1\)
+
+## Zmm Data
+root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuHLTEff\",0,0,0\)
+root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuSITEff\",8,0,0\)
+root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuStaEff\",4,0,0\)
+
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuHLTEff_tagPt\",0,0,0\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuSITEff_tagPt\",8,0,0\)
+# root -l -n -q selectProbesMuEff.C+\(\"${INPUTDIR}/Zmumu/ntuples/data_select.root\",\"${OUTPUTDIR}/Zmm/Data/MuStaEff_tagPt\",4,0,0\)
+############################################################################################################################
+############################################################################################################################
+######################## Electrons #############
+
+# These are the ones for low PU 13 TeV
+# mc
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleHLTEff\",0,0,1,0\)
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleGSFSelEff\",4,0,1,0\)
+
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleHLTEff_tagPt\",0,0,1,0\)
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleGSFSelEff_tagPt\",4,0,1,0\)
+
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee_minlo/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleHLTEff_minlo\",0,0,1,0\)
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee_minlo/ntuples/zee_select.root\",\"${OUTPUTDIR}/Zee/MC/EleGSFSelEff_minlo\",4,0,1,0\)
+# # # # data
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/data_select.root\",\"${OUTPUTDIR}/Zee/Data/EleHLTEff\",0,0,0,0\)
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/data_select.root\",\"${OUTPUTDIR}/Zee/Data/EleGSFSelEff\",4,0,0,0\)
+
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/data_select.root\",\"${OUTPUTDIR}/Zee/Data/EleHLTEff_tagPt\",0,0,0,0\)
+# root -l -n -q selectProbesEleEff.C+\(\"${INPUTDIR}/Zee/ntuples/data_select.root\",\"${OUTPUTDIR}/Zee/Data/EleGSFSelEff_tagPt\",4,0,0,0\)
+
+
+# rm *.so *.d
