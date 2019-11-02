950269867d4b0a53570bdd4eac8db712f52211d0
Stephanie Brandt <s.brandt@cern.ch>
2019-11-02 19:54:48 +0100

    combine charges

--- Efficiency/TagAndProbe/runEleEff.sh
+++ Efficiency/TagAndProbe/runEleEff.sh
@@ -34,26 +34,45 @@ LUMI5=294.4
 # POSTFIX=_POWHEGxPhotos
 POSTFIX_CB=_CBxBW_v1
 
+
 #################################################################
 # Electron efficiencies
 #
 # 13 TeV shits
-# # supercluster efficiency
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
+# # # supercluster efficiency
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff/probes.root\"\)
 
 ### Different Tag Pt cut
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
-# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Negative\",\"png\",0,0,-1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_hlt.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/Data/EleHLTEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleHLTEff_aMCxPythia${POSTFIX}_tagPt/Positive\",\"png\",0,0,1,\"Supercluster\",\"supercluster\",0.0,1.15,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/HLTEff_tagPt/probes.root\"\)
 
 # # # # ####################### gsf+is+iso efficiency
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleGSFSelEff_aMCxPythia${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_aMCxPythia${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",2,7,2,7,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWBKG${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+
+### Tag Pt
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleGSFSelEff_aMCxPythia${POSTFIX}_tagPt/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff_tagPt/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff_tagPt/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_aMCxPythia${POSTFIX}_tagPt/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff_tagPt/probes.root\"\)
+
+
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",5,1,5,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWxPythia${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",6,1,6,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWxPhotos${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+
+# root -l -b -q plotEff.C+\(\"ele_gsf.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_minloxPythia${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff_minlo/probes.root\"\)
+
+
+# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,7,2,7,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWBKG${POSTFIX}/Combined\",\"png\",0,0,0,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+
+###################################################
 # root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleGSFSelEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
 # root -l -b -q plotEff.C+\(\"elfineptbins.bins\",0,0,0,0,\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/MC/EleGSFSelEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
-root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
+# root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_aMCxPythia${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
 # root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_aMCxPythia${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
 # root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,7,2,7,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWBKG${POSTFIX}/Negative\",\"png\",0,0,-1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
 # root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,7,2,7,\"${NTUPLEDIR_13}/Zee/Data/EleGSFSelEff/probes.root\",\"${OUTPUTDIR_13}/Zee/Data/EleGSFSelEff_POWBKG${POSTFIX}/Positive\",\"png\",0,0,1,\"Supercluster\",\"GSF\",0.5,1.02,${LUMI13},\"${NTUPLEDIR_13}/Zee/MC/EleGSFSelEff/probes.root\"\)
@@ -75,23 +94,20 @@ root -l -b -q plotEff.C+\(\"elfineptbins.bins\",2,1,2,2,\"${NTUPLEDIR_13}/Zee/Da
 
 TYPE=_aMCxPythia
 # # # # #### Draw Plots
- root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/pt/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/pt/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ # root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/pt/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
+ # root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/pt/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
  
- # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/eta/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/eta/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/eta/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
+ # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/eta/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
   
- # # root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${POSTFIX}/etapt/Negative\",\"$OUTPUTDIR_13/Zee/MC/HLTEff${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/HLTEff${POSTFIX}/Negative/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- # # root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${POSTFIX}/etapt/Positive\",\"$OUTPUTDIR_13/Zee/MC/HLTEff${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/HLTEff${POSTFIX}/Positive/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/etapt/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Negative/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
+ root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeHLT${TYPE}${POSTFIX}/etapt/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleHLTEff${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleHLTEff${TYPE}${POSTFIX}/Positive/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"ele_hlt.bins\"\)
  
- root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/pt/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Negative/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/pt/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Positive/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ # root -l -b -q plotDataMC_singlepTbins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/pt/Combined\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"PtBins\",0.00,1.20,$LUMI13,\"ele_gsf.bins\"\)
  
- # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/eta/Negative\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Negative/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/eta/Positive\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Positive/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ # root -l -b -q plotDataMC_singleEtabins.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/eta/Combined\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"EtaBins\",0.00,1.20,$LUMI13,\"ele_gsf.bins\"\)
  
- # root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${POSTFIX}/etapt/Negative\",\"$OUTPUTDIR_13/Zee/MC/GSFSelEff${POSTFIX}/Negative/eff.root\",\"$OUTPUTDIR_13/Zee/Data/GSFSelEff${POSTFIX}/Negative/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
- # root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${POSTFIX}/etapt/Positive\",\"$OUTPUTDIR_13/Zee/MC/GSFSelEff${POSTFIX}/Positive/eff.root\",\"$OUTPUTDIR_13/Zee/Data/GSFSelEff${POSTFIX}/Positive/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"elfineptbins.bins\"\)
+ root -l -b -q plotDataMC.C+\(\"$OUTPUTDIR_13/Plots/ZeeGSFSel${TYPE}${POSTFIX}/etapt/Combined\",\"$OUTPUTDIR_13/Zee/MC/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"$OUTPUTDIR_13/Zee/Data/EleGSFSelEff${TYPE}${POSTFIX}/Combined/eff.root\",\"EtaPtBins\",0.00,1.20,$LUMI13,\"ele_gsf.bins\"\)
 #####################################################################
 # Electron efficiencies
 ###################### REPLACE THESE / COPY 13TeV DOWN HERE
