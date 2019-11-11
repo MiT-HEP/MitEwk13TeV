c553085a575aeab35e5fa33fe674503485099f78
Stephanie Brandt <s.brandt@cern.ch>
2019-11-11 16:09:18 +0100

    change dir structure

--- Efficiency/TagAndProbe/thesisTexDump.sh
+++ Efficiency/TagAndProbe/thesisTexDump.sh
@@ -2,12 +2,17 @@
 ## Take the plot and text file table output and attempt to make the latex files so I don't kill myself instead
 
 # do the text files first....
-INDIR="/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/Efficiency/LowPU2017ID_13TeV/results"
-OUTDIR="${INDIR}/latex"
+# INDIR="/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/Efficiency/LowPU2017ID_13TeV/results"
+# COM="13"
+
+
+INDIR="/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/Efficiency/LowPU2017ID_5TeV/results"
+COM="5"
+
+
+OUTDIR="${INDIR}/latex_fixSubfig"
 
 SFX="_aMCxPythia"
-COM="13"
-# COM="5"
 
 
 mkdir -p ${OUTDIR}/fig
@@ -18,36 +23,41 @@ mkdir -p ${OUTDIR}/tab
 # for i in "${my_array[@]}"; do echo "$i"; done
 
 
-# LEPS=("Ele" "Mu")
-# LEPWS=("electrons" "muons")
-# Q=("Combined")
-# CAT_ELE=("GSFSel")
-# CAT_MU=("SIT" "Sta")
-# # CAT_MU=("HLT")
-# PFX=("Zee" "Zmm")
-# SAMP=("Data" "MC")
+LEPS=("Ele" "Mu")
+LEPWS=("electrons" "muons")
+Q=("Combined")
+CAT_ELE=("GSFSel")
+CAT_MU=("SIT" "Sta")
+CAP_ELE=("GSF electron identification and isolation")
+CAP_MU=("Muon selection" "Standalone muon identification")
+# CAT_MU=("HLT")
+PFX=("Zee" "Zmm")
+SAMP=("Data" "MC")
+CAPQ=("combined")
 
 # # # # LEPS=("Mu")
 # # # # LEPWS=("muons")
 # # # # Q=("Combined")
 # # # # CAT_MU=("Sta")
 # # # # PFX=("Zmm")
-LEPS=("Ele" "Mu")
-LEPWS=("electrons" "muons")
-Q=("Positive" "Negative")
-CAT_ELE=("HLT")
-# CAT_MU=("Sel" "Sta" "HLT")
-CAT_MU=("HLT")
-PFX=("Zee" "Zmm")
-SAMP=("Data" "MC")
+
+# LEPS=("Ele" "Mu")
+# LEPWS=("electrons" "muons")
+# Q=("Positive" "Negative")
+# CAT_ELE=("HLT")
+# # CAT_MU=("Sel" "Sta" "HLT")
+# CAT_MU=("HLT")
+# CAP_ELE=("Single electron trigger")
+# CAP_MU=("Single muon trigger")
+# PFX=("Zee" "Zmm")
+# SAMP=("Data" "MC")
+# CAPQ=("positively" "negatively")
 
 TXTF="PtBins_scalefactors_niceTable.txt"
-TABPFX="tab/tab.07.04.Eff"
-FIGPFX="fig/fig.07.04.Eff"
+TABPFX="tab/tab-EffSF"
+FIGPFX="fig/fig-EffSF"
 ## Loops to set up the directory paths 
-CAP_ELE=("GSF electron identification and isolation" "Single electron trigger")
-CAP_MU=("Muon selection" "Standalone muon identification" "Single muon trigger")
-CAPQ=("positively" "negatively")
+
 for ((i=0;i<${#LEPS[@]};++i)); do # BEGIN loop through lepton flavors
   flav=${LEPS[i]}
   echo ${flav}
@@ -64,16 +74,16 @@ for ((i=0;i<${#LEPS[@]};++i)); do # BEGIN loop through lepton flavors
   for ((j=0;j<${#CAT[@]};++j)); do
     cat=${CAT[j]}
     # Make a new .tex file which holds the table for the + and - tables for this bin
-    TABLEFILE="${OUTDIR}/${TABPFX}.${flav}.${COM}TeV.${cat}.tex"
+    TABLEFILE="${OUTDIR}/${TABPFX}-${flav}-${COM}-${cat}.tex"
     echo "%%%% Tables for ${flav}${cat} Efficiency  %%%%%" > ${TABLEFILE}
     # Make a new .tex file that holds the figures for + and - for this thing
-    PLOTSFILE="${OUTDIR}/${FIGPFX}.${flav}.${COM}TeV.${cat}.tex"
+    PLOTSFILE="${OUTDIR}/${FIGPFX}-${flav}-${COM}-${cat}.tex"
     echo "%%%% Figures for ${flav}${cat} Efficiency  %%%%%" > ${PLOTSFILE}
     
     echo ${TABLEFILE}
     for ((k=0;k<${#Q[@]};++k)); do # BEGIN loop through charges
       q=${Q[k]}
-      PLOTDIR="figures/${COM}TeV/${pfx}${cat}${SFX}/${q}"
+      PLOTDIR="${COM}_${pfx}${cat}_${q}"
       PLOTDIR=${PLOTDIR,,}
       mkdir -p ${OUTDIR}/${PLOTDIR}
       echo ${OUTDIR}/${PLOTDIR}
@@ -86,7 +96,7 @@ for ((i=0;i<${#LEPS[@]};++i)); do # BEGIN loop through lepton flavors
       echo "directory name is  ${INDIR}/Plots/${pfx}${cat}${SFX}/pt/${q}"
       echo "\begin{table}%[htbp]" >> ${TABLEFILE}
       echo "\begin{center}" >> ${TABLEFILE}
-      echo "\scalebox{0.7}{" >> ${TABLEFILE}
+      echo "\scalebox{0.6}{" >> ${TABLEFILE}
       ## Add the file that has the formatting & data for the double-row table that doesn't look like shit
       cat "${INDIR}/Plots/${pfx}${cat}${SFX}/pt/${q}/${TXTF}" >> ${TABLEFILE}
       echo "\end{center}" >> ${TABLEFILE}
@@ -100,28 +110,27 @@ for ((i=0;i<${#LEPS[@]};++i)); do # BEGIN loop through lepton flavors
       # get a list of only the .png files in the directory
       
       echo "\begin{figure}" >> ${PLOTSFILE}
+      echo "\centering" >> ${PLOTSFILE}
       PLOTS=( $( ls ${INDIR}/Plots/${pfx}${cat}${SFX}/pt/${q}/ | grep ".png") )
       for ((m=0;m<${#PLOTS[@]};++m)); do
         echo ${PLOTS[m]}
-        echo "\centering" >> ${PLOTSFILE}
-        echo "\begin{subfigure}{.33\textwidth}" >> ${PLOTSFILE}
-        echo "\centering" >> ${PLOTSFILE}
-        PLOTNAME="ch07/figures/${COM}TeV/${pfx}${cat}${SFX}/${q}"
+        # echo "\begin{subfigure}{.33\textwidth}" >> ${PLOTSFILE}
+        # echo "\centering" >> ${PLOTSFILE}
+        PLOTNAME="plots/efficiency/${PLOTDIR}"
         PLOTNAME=${PLOTNAME,,} # lowercase that shit because Overleaf keeps ruining it randomly
         echo ${PLOTNAME}
-        echo "\includegraphics[width=\linewidth]{${PLOTNAME}/${PLOTS[m]}}" >> ${PLOTSFILE}
-        echo "%   \caption{1a}" >> ${PLOTSFILE}
-        echo "%   \label{fig:Eff:${LEPWS[i]:0:2}:${COM}TeV:${cat}:${CAPQ[k]:0:3}" >> ${PLOTSFILE}
-        echo "\end{subfigure}%" >> ${PLOTSFILE}
-        echo $(( ${m} % 2 ))
-        if [[ $(( ${m} % 3 )) = 0 ]] || [[ $(( ${m} % 3 )) = 1 ]]; then
-          echo "hello"
+        echo "\includegraphics[width=0.32\linewidth]{${PLOTNAME}/${PLOTS[m]}}" >> ${PLOTSFILE}
+        # echo "\end{subfigure}%" >> ${PLOTSFILE}
+        # echo $(( ${m} % 2 ))
+        # if [[ $(( ${m} % 3 )) = 0 ]] || [[ $(( ${m} % 3 )) = 1 ]]; then
+          # echo "hello"
           # echo "\hfill" >> ${PLOTSFILE}
-        elif [[ $(( ${m} % 3 )) = 2 ]]; then
-          echo "\\\\ " >> ${PLOTSFILE}
-        fi
+        # elif [[ $(( ${m} % 3 )) = 2 ]]; then
+          # echo "\\\\ " >> ${PLOTSFILE}
+        # fi
       done # END loop through pT bins
       echo "\caption{\$\eta\$ dependence of ${CAP[j]} efficiency scale factors, separated by \$p_T\$ bins, for ${CAPQ[k]} charged ${LEPWS[i]} in the ${COM} TeV samples.}" >> ${PLOTSFILE}
+      echo "\label{fig:Eff:${LEPWS[i]:0:2}:${COM}:${cat}:${CAPQ[k]:0:3}}" >> ${PLOTSFILE}
       echo "\end{figure}" >> ${PLOTSFILE}
       
       
