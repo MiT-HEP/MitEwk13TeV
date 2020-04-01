bed9258117b422f0a93e18913352c71b961d4558
Stephanie Brandt <s.brandt@cern.ch>
2020-04-01 23:46:19 +0200

    remove unused or obsolete vars

--- Selection/selectWm.C
+++ Selection/selectWm.C
@@ -113,9 +113,9 @@ void selectWm(const TString conf       ="wm.conf", // input file
   //
   UInt_t  runNum, lumiSec, evtNum;
   UInt_t  npv, npu;
-  UInt_t  id_1, id_2;
-  Double_t x_1, x_2, xPDF_1, xPDF_2;
-  Double_t scalePDF, weightPDF;
+  // UInt_t  id_1, id_2;
+  // Double_t x_1, x_2, xPDF_1, xPDF_2;
+  // Double_t scalePDF, weightPDF;
   TLorentzVector *genV=0, *genLep=0, *genNu = 0;//, *genMuonMatch=0;
   Float_t genMuonPt;
   Float_t genVPt, genVPhi, genVy, genVMass;
@@ -125,10 +125,10 @@ void selectWm(const TString conf       ="wm.conf", // input file
   Float_t prefireWeight=1, prefireUp=1,    prefireDown=1;
   Float_t prefirePhoton=1, prefirePhotUp=1, prefirePhotDown=1;
   Float_t prefireJet=1,    prefireJetUp=1,  prefireJetDown=1;
-  Float_t met, metPhi, sumEt, mt, u1, u2;
-  Float_t tkMet, tkMetPhi, tkSumEt, tkMt, tkU1, tkU2;
-  Float_t mvaMet, mvaMetPhi, mvaSumEt, mvaMt, mvaU1, mvaU2;
-  Float_t puppiMet, puppiMetPhi, puppiSumEt, puppiMt, puppiU1, puppiU2;
+  Float_t met, metPhi, mt, u1, u2;
+  // Float_t tkMet, tkMetPhi, tkSumEt, tkMt, tkU1, tkU2;
+  // Float_t mvaMet, mvaMetPhi, mvaSumEt, mvaMt, mvaU1, mvaU2;
+  Float_t puppiMet, puppiMetPhi, puppiMt, puppiU1, puppiU2;
   Int_t   q;
   TLorentzVector *lep=0;
   Int_t lepID;
@@ -199,14 +199,14 @@ void selectWm(const TString conf       ="wm.conf", // input file
     outTree->Branch("evtNum",     &evtNum,     "evtNum/i");      // event number
     outTree->Branch("npv",        &npv,        "npv/i");         // number of primary vertices
     outTree->Branch("npu",        &npu,        "npu/i");         // number of in-time PU events (MC)
-    outTree->Branch("id_1",       &id_1,       "id_1/i");        // PDF info -- parton ID for parton 1
-    outTree->Branch("id_2",       &id_2,       "id_2/i");        // PDF info -- parton ID for parton 2
-    outTree->Branch("x_1",        &x_1,        "x_1/d");         // PDF info -- x for parton 1
-    outTree->Branch("x_2",        &x_2,        "x_2/d");         // PDF info -- x for parton 2
-    outTree->Branch("xPDF_1",     &xPDF_1,     "xPDF_1/d");      // PDF info -- x*F for parton 1
-    outTree->Branch("xPDF_2",     &xPDF_2,     "xPDF_2/d");      // PDF info -- x*F for parton 2
-    outTree->Branch("scalePDF",   &scalePDF,   "scalePDF/d");    // PDF info -- energy scale of parton interaction
-    outTree->Branch("weightPDF",  &weightPDF,  "weightPDF/d");   // PDF info -- PDF weight
+    // outTree->Branch("id_1",       &id_1,       "id_1/i");        // PDF info -- parton ID for parton 1
+    // outTree->Branch("id_2",       &id_2,       "id_2/i");        // PDF info -- parton ID for parton 2
+    // outTree->Branch("x_1",        &x_1,        "x_1/d");         // PDF info -- x for parton 1
+    // outTree->Branch("x_2",        &x_2,        "x_2/d");         // PDF info -- x for parton 2
+    // outTree->Branch("xPDF_1",     &xPDF_1,     "xPDF_1/d");      // PDF info -- x*F for parton 1
+    // outTree->Branch("xPDF_2",     &xPDF_2,     "xPDF_2/d");      // PDF info -- x*F for parton 2
+    // outTree->Branch("scalePDF",   &scalePDF,   "scalePDF/d");    // PDF info -- energy scale of parton interaction
+    // outTree->Branch("weightPDF",  &weightPDF,  "weightPDF/d");   // PDF info -- PDF weight
     outTree->Branch("genV",       "TLorentzVector", &genV);      // GEN boson 4-vector (signal MC)
     outTree->Branch("genLep",     "TLorentzVector", &genLep);    // GEN lepton 4-vector (signal MC)
     outTree->Branch("genNu",      "TLorentzVector", &genNu);    // GEN lepton 4-vector (signal MC)
@@ -234,25 +234,25 @@ void selectWm(const TString conf       ="wm.conf", // input file
     outTree->Branch("scale1fbDown",   &scale1fbDown,   "scale1fbDown/F");    // event weight per 1/fb (MC)
     outTree->Branch("met",        &met,        "met/F");         // MET
     outTree->Branch("metPhi",     &metPhi,     "metPhi/F");      // phi(MET)
-    outTree->Branch("sumEt",      &sumEt,      "sumEt/F");       // Sum ET
+    // outTree->Branch("sumEt",      &sumEt,      "sumEt/F");       // Sum ET
     outTree->Branch("mt",         &mt,         "mt/F");          // transverse mass
     outTree->Branch("u1",         &u1,         "u1/F");          // parallel component of recoil
     outTree->Branch("u2",         &u2,         "u2/F");          // perpendicular component of recoil
-    outTree->Branch("tkMet",      &tkMet,      "tkMet/F");       // MET (track MET)
-    outTree->Branch("tkMetPhi",   &tkMetPhi,   "tkMetPhi/F");    // phi(MET) (track MET)
-    outTree->Branch("tkSumEt",    &tkSumEt,    "tkSumEt/F");     // Sum ET (track MET)
-    outTree->Branch("tkMt",       &tkMt,       "tkMt/F");        // transverse mass (track MET)
-    outTree->Branch("tkU1",       &tkU1,       "tkU1/F");        // parallel component of recoil (track MET)
-    outTree->Branch("tkU2",       &tkU2,       "tkU2/F");        // perpendicular component of recoil (track MET)
-    outTree->Branch("mvaMet",     &mvaMet,     "mvaMet/F");      // MVA MET
-    outTree->Branch("mvaMetPhi",  &mvaMetPhi,  "mvaMetPhi/F");   // phi(MVA MET)
-    outTree->Branch("mvaSumEt",   &mvaSumEt,   "mvaSumEt/F");    // Sum ET (mva MET)
-    outTree->Branch("mvaMt",      &mvaMt,      "mvaMt/F");      // transverse mass (MVA MET)
-    outTree->Branch("mvaU1",      &mvaU1,      "mvaU1/F");       // parallel component of recoil (mva MET)
-    outTree->Branch("mvaU2",      &mvaU2,      "mvaU2/F");       // perpendicular component of recoil (mva MET)
+    // outTree->Branch("tkMet",      &tkMet,      "tkMet/F");       // MET (track MET)
+    // outTree->Branch("tkMetPhi",   &tkMetPhi,   "tkMetPhi/F");    // phi(MET) (track MET)
+    // outTree->Branch("tkSumEt",    &tkSumEt,    "tkSumEt/F");     // Sum ET (track MET)
+    // outTree->Branch("tkMt",       &tkMt,       "tkMt/F");        // transverse mass (track MET)
+    // outTree->Branch("tkU1",       &tkU1,       "tkU1/F");        // parallel component of recoil (track MET)
+    // outTree->Branch("tkU2",       &tkU2,       "tkU2/F");        // perpendicular component of recoil (track MET)
+    // outTree->Branch("mvaMet",     &mvaMet,     "mvaMet/F");      // MVA MET
+    // outTree->Branch("mvaMetPhi",  &mvaMetPhi,  "mvaMetPhi/F");   // phi(MVA MET)
+    // outTree->Branch("mvaSumEt",   &mvaSumEt,   "mvaSumEt/F");    // Sum ET (mva MET)
+    // outTree->Branch("mvaMt",      &mvaMt,      "mvaMt/F");      // transverse mass (MVA MET)
+    // outTree->Branch("mvaU1",      &mvaU1,      "mvaU1/F");       // parallel component of recoil (mva MET)
+    // outTree->Branch("mvaU2",      &mvaU2,      "mvaU2/F");       // perpendicular component of recoil (mva MET)
     outTree->Branch("puppiMet",    &puppiMet,   "puppiMet/F");      // Puppi MET
     outTree->Branch("puppiMetPhi", &puppiMetPhi,"puppiMetPhi/F");   // phi(Puppi MET)
-    outTree->Branch("puppiSumEt",  &puppiSumEt, "puppiSumEt/F");    // Sum ET (Puppi MET)
+    // outTree->Branch("puppiSumEt",  &puppiSumEt, "puppiSumEt/F");    // Sum ET (Puppi MET)
     outTree->Branch("puppiU1",     &puppiU1,    "puppiU1/F");       // parallel component of recoil (Puppi MET)
     outTree->Branch("puppiU2",     &puppiU2,    "puppiU2/F");       // perpendicular component of recoil (Puppi MET)
     outTree->Branch("q",          &q,          "q/I");           // lepton charge
@@ -513,18 +513,18 @@ void selectWm(const TString conf       ="wm.conf", // input file
 	  genNuPhi = -999;
 	  u1        = -999;
 	  u2        = -999;
-	  tkU1      = -999;
-    tkU2      = -999;
-	  mvaU1     = -999;
-    mvaU2     = -999;
-    id_1      = -999;
-    id_2      = -999;
-    x_1       = -999;
-    x_2       = -999;
-    xPDF_1    = -999;
-    xPDF_2    = -999;
-    scalePDF  = -999;
-    weightPDF = -999;
+	  // tkU1      = -999;
+    // tkU2      = -999;
+	  // mvaU1     = -999;
+    // mvaU2     = -999;
+    // id_1      = -999;
+    // id_2      = -999;
+    // x_1       = -999;
+    // x_2       = -999;
+    // xPDF_1    = -999;
+    // xPDF_2    = -999;
+    // scalePDF  = -999;
+    // weightPDF = -999;
 
     genMuonPt = 0;
     if(hasGen) genMuonPt = toolbox::getGenLep(genPartArr, vLep);
@@ -574,15 +574,15 @@ void selectWm(const TString conf       ="wm.conf", // input file
         u1 = ((vWPt.Px())*(vU.Px()) + (vWPt.Py())*(vU.Py()))/(genVPt);  // u1 = (pT . u)/|pT|
         u2 = ((vWPt.Px())*(vU.Py()) - (vWPt.Py())*(vU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
         
-        TVector2 vTkMet((info->trkMET)*cos(info->trkMETphi), (info->trkMET)*sin(info->trkMETphi));
-        TVector2 vTkU = -1.0*(vTkMet+vLepPt);
-        tkU1 = ((vWPt.Px())*(vTkU.Px()) + (vWPt.Py())*(vTkU.Py()))/(genVPt);  // u1 = (pT . u)/|pT|
-        tkU2 = ((vWPt.Px())*(vTkU.Py()) - (vWPt.Py())*(vTkU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
+        // TVector2 vTkMet((info->trkMET)*cos(info->trkMETphi), (info->trkMET)*sin(info->trkMETphi));
+        // TVector2 vTkU = -1.0*(vTkMet+vLepPt);
+        // tkU1 = ((vWPt.Px())*(vTkU.Px()) + (vWPt.Py())*(vTkU.Py()))/(genVPt);  // u1 = (pT . u)/|pT|
+        // tkU2 = ((vWPt.Px())*(vTkU.Py()) - (vWPt.Py())*(vTkU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
 
-        TVector2 vMvaMet((info->mvaMET)*cos(info->mvaMETphi), (info->mvaMET)*sin(info->mvaMETphi));
-        TVector2 vMvaU = -1.0*(vMvaMet+vLepPt);
-        mvaU1 = ((vWPt.Px())*(vMvaU.Px()) + (vWPt.Py())*(vMvaU.Py()))/(genVPt);  // u1 = (pT . u)/|pT|
-        mvaU2 = ((vWPt.Px())*(vMvaU.Py()) - (vWPt.Py())*(vMvaU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
+        // TVector2 vMvaMet((info->mvaMET)*cos(info->mvaMETphi), (info->mvaMET)*sin(info->mvaMETphi));
+        // TVector2 vMvaU = -1.0*(vMvaMet+vLepPt);
+        // mvaU1 = ((vWPt.Px())*(vMvaU.Px()) + (vWPt.Py())*(vMvaU.Py()))/(genVPt);  // u1 = (pT . u)/|pT|
+        // mvaU2 = ((vWPt.Px())*(vMvaU.Py()) - (vWPt.Py())*(vMvaU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
         
         TVector2 vPuppiMet((info->puppET)*cos(info->puppETphi), (info->puppET)*sin(info->puppETphi));
         TVector2 vPuppiU = -1.0*(vPuppiMet+vLepPt);
@@ -590,14 +590,14 @@ void selectWm(const TString conf       ="wm.conf", // input file
         puppiU2 = ((vWPt.Px())*(vPuppiU.Py()) - (vWPt.Py())*(vPuppiU.Px()))/(genVPt);  // u2 = (pT x u)/|pT|
   
       }
-      id_1      = gen->id_1;
-      id_2      = gen->id_2;
-      x_1       = gen->x_1;
-      x_2       = gen->x_2;
-      xPDF_1    = gen->xPDF_1;
-      xPDF_2    = gen->xPDF_2;
-      scalePDF  = gen->scalePDF;
-      weightPDF = gen->weight;
+      // id_1      = gen->id_1;
+      // id_2      = gen->id_2;
+      // x_1       = gen->x_1;
+      // x_2       = gen->x_2;
+      // xPDF_1    = gen->xPDF_1;
+      // xPDF_2    = gen->xPDF_2;
+      // scalePDF  = gen->scalePDF;
+      // weightPDF = gen->weight;
 
       // Clean up
       delete gvec;
@@ -610,19 +610,19 @@ void selectWm(const TString conf       ="wm.conf", // input file
     scale1fbDown = weightDown;
     met	   = info->pfMETC;
 	  metPhi   = info->pfMETCphi;
-	  sumEt    = 0;
+	  // sumEt    = 0;
 	  mt       = sqrt( 2.0 * (vLep.Pt()) * (info->pfMETC) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->pfMETCphi))) );
-	  tkMet    = info->trkMET;
-	  tkMetPhi = info->trkMETphi;
-	  tkSumEt  = 0;
-	  tkMt     = sqrt( 2.0 * (vLep.Pt()) * (info->trkMET) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->trkMETphi))) );
-	  mvaMet   = info->mvaMET;
-	  mvaMetPhi = info->mvaMETphi;
-	  mvaSumEt  = 0;
-	  mvaMt     = sqrt( 2.0 * (vLep.Pt()) * (info->mvaMET) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->mvaMETphi))) );
+	  // tkMet    = info->trkMET;
+	  // tkMetPhi = info->trkMETphi;
+	  // tkSumEt  = 0;
+	  // tkMt     = sqrt( 2.0 * (vLep.Pt()) * (info->trkMET) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->trkMETphi))) );
+	  // mvaMet   = info->mvaMET;
+	  // mvaMetPhi = info->mvaMETphi;
+	  // mvaSumEt  = 0;
+	  // mvaMt     = sqrt( 2.0 * (vLep.Pt()) * (info->mvaMET) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->mvaMETphi))) );
     puppiMet = info->puppET;
     puppiMetPhi = info->puppETphi;
-	  puppiSumEt  = 0;
+	  // puppiSumEt  = 0;
 	  puppiMt     = sqrt( 2.0 * (vLep.Pt()) * (info->puppET) * (1.0-cos(toolbox::deltaPhi(vLep.Phi(),info->puppETphi))) );
 	  q        = goodMuon->q;
 	  lep      = &vLep;
