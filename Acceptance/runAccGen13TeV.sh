d3649c3dbf2cb560d9347740349a33a3068f855e
Stephanie Brandt <s.brandt@cern.ch>
2020-04-02 19:29:27 +0200

    full

--- Acceptance/runAccGen13TeV.sh
+++ Acceptance/runAccGen13TeV.sh
@@ -1,90 +1,116 @@
 #! /bin/bash
 
-OUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/Acceptance/GenAcc13_Powheg_Unc
-
-## minlo and powheg
-## Undressed
-# root -l -q computeAccGenWm_Sys.C+\(\"wmp_13_powheg.conf\",\"${OUTDIR}/WmpGen13TeV_powheg_Undressed\",0,1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wmm_13_powheg.conf\",\"${OUTDIR}/WmmGen13TeV_powheg_Undressed\",0,-1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmGen13TeV_powheg_Undressed\",0,0\)
-
-## Dressed
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmpGen13TeV_Dressed\",1,1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmmGen13TeV_Dressed\",1,-1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmGen13TeV_Dressed\",1,0\)
+OUTDIR=/afs/cern.ch/user/s/sabrandt/work/public/FilesSM2017GH/Acceptance/FINAL_GEN/V
 
 
-# # W->enu
-# # #
-# root -l -q computeAccGenWe_Sys.C+\(\"wep_13_powheg.conf\",\"${OUTDIR}/WepGen13TeV_powheg_Undressed\",0,1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wem_13_powheg.conf\",\"${OUTDIR}/WemGen13TeV_powheg_Undressed\",0,-1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WeGen13TeV_Undressed\",0,0\)
+########################################
+##             W->munu
+########################################
 
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WepGen13TeV_Dressed\",1,1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WemGen13TeV_Dressed\",1,-1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WeGen13TeV_Dressed\",1,0\)
+# # ################################################################################
+# ##########     aMCnlo+Pythia for RESUMMATION, QCD, and PDF
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wmp_13TeV_amcPythia_dressed\",\"ACC_wmp_13_amcPythia_dressed\",1,1\)
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wmm_13TeV_amcPythia_dressed\",\"ACC_wmm_13_amcPythia_dressed\",1,-1\)
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wm_13TeV_amcPythia_dressed\",\"ACC_wm_13_amcPythia_dressed\",1,0\)
 
-#
-# Z->mm
-#
+# # ################################################################################
+# ##########     aMCnlo+Pythia with pT reweight for RESUMMATION
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wmp_13TeV_amcPythia_ptWeight\",\"ACC_wmp_13_amcPythia_ptWeight\",1,1\)
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wmm_13TeV_amcPythia_ptWeight\",\"ACC_wmm_13_amcPythia_ptWeight\",1,-1\)
+root -l -q computeAccGenWm.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wm_13TeV_amcPythia_ptWeight\",\"ACC_wm_13_amcPythia_ptWeight\",1,0\)
 
-root -l -q computeAccGenZmm_Sys.C+\(\"zmm_13_minlo.conf\",\"${OUTDIR}/ZmmGen13TeV_minlo_Undressed\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"zee_13_minlo.conf\",\"${OUTDIR}/ZmmGen13TeV_Minlo_UnDressed_2017\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZmmGen13TeV_UnDressed_2017\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"zmm_2015.conf\",\"${OUTDIR}/ZmmGen13TeV_UnDressed_2015\",0\)
+# # ################################################################################
+# ##########     Powheg+Pythia for RESUMMATION
+root -l -q computeAccGenWm.C+\(\"wm13_powheg.conf\",\"${OUTDIR}/GEN_wmp_13TeV_powPythia_dressed\",\"ACC_wmp_13_powPythia_dressed\",1,1\)
+root -l -q computeAccGenWm.C+\(\"wm13_powheg.conf\",\"${OUTDIR}/GEN_wmm_13TeV_powPythia_dressed\",\"ACC_wmm_13_powPythia_dressed\",1,-1\)
+root -l -q computeAccGenWm.C+\(\"wm13_powheg.conf\",\"${OUTDIR}/GEN_wm_13TeV_powPythia_dressed\",\"ACC_wm_13_powPythia_dressed\",1,0\)
 
-#
-# Z->ee
-#
-
-# root -l -q computeAccGenZee_Sys.C+\(\"zee_13_minlo.conf\",\"${OUTDIR}/ZeeGen13TeV_minlo_Undressed\",0\)
-# root -l -q computeAccGenZee_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZeeGen13TeV_Dressed\",1\)
-
-
-
-#########################################
-###   regular aMC@nlo
-#########################################
+# # ################################################################################
+# ##########     Powheg+Photos for EWK
+root -l -q computeAccGenWm.C+\(\"wm13_photos.conf\",\"${OUTDIR}/GEN_wmp_13TeV_powPhotos_undressed\",\"ACC_wmp_13_powPhotos_undressed\",0,1\)
+root -l -q computeAccGenWm.C+\(\"wm13_photos.conf\",\"${OUTDIR}/GEN_wmm_13TeV_powPhotos_undressed\",\"ACC_wmm_13_powPhotos_undressed\",0,-1\)
+root -l -q computeAccGenWm.C+\(\"wm13_photos.conf\",\"${OUTDIR}/GEN_wm_13TeV_powPhotos_undressed\",\"ACC_wm_13_powPhotos_undressed\",0,0\)
 
 
-
-#
-# W->munu
-# #
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmpGen13TeV_Undressed\",0,1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmmGen13TeV_Undressed\",0,-1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmGen13TeV_Undressed\",0,0\)
-
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmpGen13TeV_Dressed\",1,1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmmGen13TeV_Dressed\",1,-1\)
-# root -l -q computeAccGenWm_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WmGen13TeV_Dressed\",1,0\)
+# # ################################################################################
+# # ##########     Powheg+Pythia for EWK
+root -l -q computeAccGenWm.C+\(\"wm13_pythia.conf\",\"${OUTDIR}/GEN_wmp_13TeV_powPythia_undressed\",\"ACC_wmp_13_powPythia_undressed\",0,1\)
+root -l -q computeAccGenWm.C+\(\"wm13_pythia.conf\",\"${OUTDIR}/GEN_wmm_13TeV_powPythia_undressed\",\"ACC_wmm_13_powPythia_undressed\",0,-1\)
+root -l -q computeAccGenWm.C+\(\"wm13_pythia.conf\",\"${OUTDIR}/GEN_wm_13TeV_powPythia_undressed\",\"ACC_wm_13_powPythia_undressed\",0,0\)
 
 
-# # W->enu
-# #
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WepGen13TeV_Undressed\",0,1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WemGen13TeV_Undressed\",0,-1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WeGen13TeV_Undressed\",0,0\)
+########################################
+##             W->enu
+########################################
 
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WepGen13TeV_Dressed\",1,1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WemGen13TeV_Dressed\",1,-1\)
-# root -l -q computeAccGenWe_Sys.C+\(\"wj_13.conf\",\"${OUTDIR}/WeGen13TeV_Dressed\",1,0\)
+# ################################################################################
+##########     aMCnlo+Pythia for RESUMMATION, QCD, and PDF
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wep_13TeV_amcPythia_dressed\",\"ACC_wep_13_amcPythia_dressed\",1,1\)
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wem_13TeV_amcPythia_dressed\",\"ACC_wem_13_amcPythia_dressed\",1,-1\)
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_we_13TeV_amcPythia_dressed\",\"ACC_we_13_amcPythia_dressed\",1,0\)
 
-#
-# Z->mm
-#
+# ################################################################################
+##########     aMCnlo+Pythia for RESUMMATION with pT weight
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wep_13TeV_amcPythia_ptWeight\",\"ACC_wep_13_amcPythia_ptWeight\",1,1\)
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_wem_13TeV_amcPythia_ptWeight\",\"ACC_wem_13_amcPythia_ptWeight\",1,-1\)
+root -l -q computeAccGenWe.C+\(\"w13.conf\",\"${OUTDIR}/GEN_we_13TeV_amcPythia_ptWeight\",\"ACC_we_13_amcPythia_ptWeight\",1,0\)
 
-# root -l -q computeAccGenZmm_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZmmGen13TeV_Undressed\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"zee_13_minlo.conf\",\"${OUTDIR}/ZmmGen13TeV_Minlo_UnDressed_2017\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZmmGen13TeV_UnDressed_2017\",0\)
-# root -l -q computeAccGenZmm_Sys.C+\(\"zmm_2015.conf\",\"${OUTDIR}/ZmmGen13TeV_UnDressed_2015\",0\)
+# ################################################################################
+##########     Powheg+Pythia for RESUMMATION
+root -l -q computeAccGenWe.C+\(\"we13_powheg.conf\",\"${OUTDIR}/GEN_wep_13TeV_powPythia_dressed\",\"ACC_wep_13_powPythia_dressed\",1,1\)
+root -l -q computeAccGenWe.C+\(\"we13_powheg.conf\",\"${OUTDIR}/GEN_wem_13TeV_powPythia_dressed\",\"ACC_wem_13_powPythia_dressed\",1,-1\)
+root -l -q computeAccGenWe.C+\(\"we13_powheg.conf\",\"${OUTDIR}/GEN_we_13TeV_powPythia_dressed\",\"ACC_we_13_powPythia_dressed\",1,0\)
 
-#
-# Z->ee
-#
+# ################################################################################
+##########     Powheg+Photos for EWK
+root -l -q computeAccGenWe.C+\(\"we13_photos.conf\",\"${OUTDIR}/GEN_wep_13TeV_powPhotos_undressed\",\"ACC_wep_13_powPhotos_undressed\",1,1\)
+root -l -q computeAccGenWe.C+\(\"we13_photos.conf\",\"${OUTDIR}/GEN_wem_13TeV_powPhotos_undressed\",\"ACC_wem_13_powPhotos_undressed\",1,-1\)
+root -l -q computeAccGenWe.C+\(\"we13_photos.conf\",\"${OUTDIR}/GEN_we_13TeV_powPhotos_undressed\",\"ACC_we_13_powPhotos_undressed\",1,0\)
 
-# root -l -q computeAccGenZee_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZeeGen13TeV_Undressed\",0\)
-# root -l -q computeAccGenZee_Sys.C+\(\"z_13.conf\",\"${OUTDIR}/ZeeGen13TeV_Dressed\",1\)
+# # ################################################################################
+# ##########     Powheg+Pythia for EWK
+root -l -q computeAccGenWe.C+\(\"we13_pythia.conf\",\"${OUTDIR}/GEN_wep_13TeV_powPythia_undressed\",\"ACC_wep_13_powPythia_undressed\",1,1\)
+root -l -q computeAccGenWe.C+\(\"we13_pythia.conf\",\"${OUTDIR}/GEN_wem_13TeV_powPythia_undressed\",\"ACC_wem_13_powPythia_undressed\",1,-1\)
+root -l -q computeAccGenWe.C+\(\"we13_pythia.conf\",\"${OUTDIR}/GEN_we_13TeV_powPythia_undressed\",\"ACC_we_13_powPythia_undressed\",1,0\)
 
+
+########################################
+##             Z->mm
+########################################
+
+# ################################################################################
+# #########             aMCnlo+Pythia for RESUMMATION, QCD, and PDF
+root -l -q computeAccGenZmm.C+\(\"z13.conf\",\"${OUTDIR}/GEN_zmm_13TeV_amcPythia_dressed\",\"ACC_zmm_13_amcPythia_dressed\",1\)
+# ################################################################################
+# #########             aMCnlo+Pythia for RESUMMATION with pT weights
+root -l -q computeAccGenZmm.C+\(\"z13.conf\",\"${OUTDIR}/GEN_zmm_13TeV_amcPythia_ptWeight\",\"ACC_zmm_13_amcPythia_ptWeight\",1\)
+# # ################################################################################
+# # #########             Powheg+minlo+Pythia for RESUMMATION
+root -l -q computeAccGenZmm.C+\(\"zmm13_minlo.conf\",\"${OUTDIR}/GEN_zmm_13TeV_powPythia_dressed\",\"ACC_zmm_13_powPythia_dressed\",1\)
+# # ################################################################################
+# # ##########            Powheg+Photos for EWK
+root -l -q computeAccGenZmm.C+\(\"zmm13_photos.conf\",\"${OUTDIR}/GEN_zmm_13TeV_powPhotos_undressed\",\"ACC_zmm_13_powPhotos_undressed\",0\)
+# # ################################################################################
+# # #########             Powheg+Pythia for EWK
+root -l -q computeAccGenZmm.C+\(\"zmm13_pythia.conf\",\"${OUTDIR}/GEN_zmm_13TeV_powPythia_undressed\",\"ACC_zmm_13_powPythia_undressed\",0\)
+
+
+########################################
+##             Z->ee
+########################################
+# ################################################################################
+# # ##########             aMCnlo+Pythia for RESUMMATION
+root -l -q computeAccGenZee.C+\(\"z13.conf\",\"${OUTDIR}/GEN_zee_13TeV_amcPythia_dressed\",\"ACC_zee_13_amcPythia_dressed\",1\)# ################################################################################
+# # ##########             aMCnlo+Pythia for RESUMMATION
+root -l -q computeAccGenZee.C+\(\"z13.conf\",\"${OUTDIR}/GEN_zee_13TeV_amcPythia_ptWeight\",\"ACC_zee_13_amcPythia_ptWeight\",1\)
+# ################################################################################
+# # ##########             Powheg+minlo+Pythia for RESUMMATION
+root -l -q computeAccGenZee.C+\(\"zee13_minlo.conf\",\"${OUTDIR}/GEN_zee_13TeV_powPythia_dressed\",\"ACC_zee_13_powPythia_dressed\",1\)
+################################################################################
+# # ##########             Powheg+Photos for EWK
+root -l -q computeAccGenZee.C+\(\"zee13_photos.conf\",\"${OUTDIR}/GEN_zee_13TeV_powPhotos_undressed\",\"ACC_zee_13_powPhotos_undressed\",0\)
+################################################################################
+# ##########             Powheg+Pythia for EWK
+root -l -q computeAccGenZee.C+\(\"zee13_pythia.conf\",\"${OUTDIR}/GEN_zee_13TeV_powPythia_undressed\",\"ACC_zee_13_powPythia_undressed\",0\)
 
 # rm *.so *.d
